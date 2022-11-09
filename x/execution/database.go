f203366912edff462de3afdb6ad14365ea9473f4
brennan lamey <brennanlamey@gmail.com>
2022-11-09 13:50:03 -0600

    basic execution

--- x/execution/database.go
+++ x/execution/database.go
@@ -2,6 +2,7 @@ package execution
 
 import (
 	"context"
+	"ksl/sqlclient"
 	"kwil/x/schema"
 )
 
@@ -12,63 +13,115 @@ type Input struct {
 	Value string
 }
 
-type ExecutionResponse struct {
-}
-
 type Service interface {
-	Execute(ctx context.Context, owner, name, query string, inputs []Input) (*ExecutionResponse, error)
+	Execute(ctx context.Context, owner, name, query string, inputs []Input) error
+	Read(ctx context.Context, owner, name, query string, inputs []Input) error
 }
 
 type executionService struct {
-	md schema.Service
+	md        schema.Service
+	connector schema.Connector
 }
 
-func NewExecutionService() *executionService {
-	return &executionService{}
+func NewExecutionService(md schema.Service, conn schema.Connector) *executionService {
+	return &executionService{
+		md:        md,
+		connector: conn,
+	}
 }
 
 /*
 There are a lot of things that need to change here
 */
-func (s *executionService) Execute(ctx context.Context, owner, name, caller, query string, inputs []Input) (*ExecutionResponse, error) {
-	md, err := s.md.GetDatabase(ctx, owner, name)
+func (s *executionService) Execute(ctx context.Context, owner, name, caller, query string, inputs []Input) error {
+	md, err := s.md.GetMetadata(ctx, schema.RequestMetadata{Wallet: owner, Database: name})
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	var q *schema.Query
 	// first, validate that the query exists in md.Queries
 	for _, qs := range md.Queries {
 		if q.Name == query {
-			q = qs
+			q = &qs
 			break
 		}
 	}
 
 	if q == nil {
-		return nil, ErrQueryNotFound
+		return ErrQueryNotFound
 	}
 
 	// we need to verify that the caller is authorized to execute the query
 	perms, err := hasPermissions(&md, &caller, &query)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	if !perms {
-		return nil, ErrUnauthorized
+		return ErrUnauthorized
 	}
 
 	// next, we need to verify the types
+
+	ins, err := validateTypes(q, &query, inputs)
+	if err != nil {
+		return err
+	}
+
+	// get connection info
+	url, err := s.connector.GetConnectionInfo(owner)
+	if err != nil {
+		return err
+	}
+
+	client, err := sqlclient.Open(ctx, url)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	// execute
+	_, err = client.DB.Exec(q.Statement, ins)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-// TODO: implement this
+// TODO: There should be a more efficient way for finding roles and queries than having to iterate through all of them
 // this will get the callers role and check if they have permissions to execute the query
-func hasPermissions(md *schema.Database, caller, query *string) (bool, error) {
+func hasPermissions(md *schema.Metadata, caller, query *string) (bool, error) {
+	// search for default role
+	var rl *schema.Role
+	for _, role := range md.Roles {
+		if role.Name == md.DefaultRole {
+			rl = &role
+			break
+		}
+	}
+
+	if rl == nil {
+		return false, ErrRoleNotFound
+	}
+	// now search for the query
+	var found bool
+	for _, qs := range md.Queries {
+		if qs.Name == *query {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		return false, ErrRoleDoesNotHavePermission
+	}
+
 	return true, nil
 }
 
 // this will try to transform the inputs into the types specified in the query
 // it will return the query with the inputs transformed
-func validateTypes(md *schema.Query, query *string, inputs []Input) (bool, error) {
-	return true, nil
+func validateTypes(md *schema.Query, query *string, inputs []Input) ([]Input, error) {
+	return inputs, nil
 }
--- x/execution/errors.go
+++ x/execution/errors.go
@@ -4,3 +4,5 @@ import "errors"
 
 var ErrUnauthorized = errors.New("unauthorized to execute query")
 var ErrQueryNotFound = errors.New("query not found")
+var ErrRoleNotFound = errors.New("role not found")
+var ErrRoleDoesNotHavePermission = errors.New("role does not have permission")
