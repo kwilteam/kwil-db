2f2e9c9adea8e77a1d7465f510487536887ffb26
pputhanv <31552955+pputhanv@users.noreply.github.com>
2023-01-17 13:31:28 -0600

    refacor the deploy and drop functions as per the cobra guidelines (#9)

--- cmd/kwil-cli/database/deploy.go
+++ cmd/kwil-cli/database/deploy.go
@@ -3,49 +3,60 @@ package database
 import (
 	"context"
 	"fmt"
+	"kwil/cmd/kwil-cli/common"
+	"kwil/cmd/kwil-cli/common/display"
+	grpc_client "kwil/kwil/client/grpc-client"
+	execUtils "kwil/x/execution/utils"
+	"os"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
-	"kwil/cmd/kwil-cli/common"
-	"kwil/cmd/kwil-cli/common/display"
-	"kwil/kwil/client/grpc-client"
-	execUtils "kwil/x/execution/utils"
-	"os"
 )
 
-func cmdDeploy(cmd *cobra.Command, args []string) error {
-	return common.DialGrpc(cmd.Context(), viper.GetViper(), func(ctx context.Context, cc *grpc.ClientConn) error {
-		if len(args) != 0 {
-			return fmt.Errorf("deploy command does not take any arguments")
-		}
+func deployCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "deploy",
+		Short: "Deploy databases",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return common.DialGrpc(cmd.Context(), viper.GetViper(), func(ctx context.Context, cc *grpc.ClientConn) error {
+				if len(args) != 0 {
+					return fmt.Errorf("deploy command does not take any arguments")
+				}
 
-		filePath, err := cmd.Flags().GetString("path")
-		if err != nil {
-			return fmt.Errorf("must specify a path path with the --path flag")
-		}
+				filePath, err := cmd.Flags().GetString("path")
+				if err != nil {
+					return fmt.Errorf("must specify a path path with the --path flag")
+				}
 
-		// read in the file
-		file, err := os.ReadFile(filePath)
-		if err != nil {
-			return fmt.Errorf("failed to read file: %w", err)
-		}
+				// read in the file
+				file, err := os.ReadFile(filePath)
+				if err != nil {
+					return fmt.Errorf("failed to read file: %w", err)
+				}
 
-		db, err := execUtils.DBFromJson(file)
-		if err != nil {
-			return fmt.Errorf("failed to parse database: %w", err)
-		}
+				db, err := execUtils.DBFromJson(file)
+				if err != nil {
+					return fmt.Errorf("failed to parse database: %w", err)
+				}
 
-		client, err := grpc_client.NewClient(cc, viper.GetViper())
-		if err != nil {
-			return fmt.Errorf("failed to create client: %w", err)
-		}
+				client, err := grpc_client.NewClient(cc, viper.GetViper())
+				if err != nil {
+					return fmt.Errorf("failed to create client: %w", err)
+				}
 
-		res, err := client.DeployDatabase(cmd.Context(), db)
-		if err != nil {
-			return err
-		}
+				res, err := client.DeployDatabase(cmd.Context(), db)
+				if err != nil {
+					return err
+				}
 
-		display.PrintTxResponse(res)
-		return nil
-	})
+				display.PrintTxResponse(res)
+				return nil
+			})
+		},
+	}
+
+	cmd.Flags().StringP("path", "p", "", "Path to the database definition file")
+	cmd.MarkFlagRequired("path")
+	return cmd
 }
--- cmd/kwil-cli/database/drop.go
+++ cmd/kwil-cli/database/drop.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"kwil/cmd/kwil-cli/common"
 	"kwil/cmd/kwil-cli/common/display"
-	"kwil/kwil/client/grpc-client"
+	grpc_client "kwil/kwil/client/grpc-client"
 	"kwil/x/types/databases"
 	"kwil/x/types/transactions"
 
@@ -14,35 +14,42 @@ import (
 	"google.golang.org/grpc"
 )
 
-func cmdDrop(cmd *cobra.Command, args []string) error {
-	return common.DialGrpc(cmd.Context(), viper.GetViper(), func(ctx context.Context, cc *grpc.ClientConn) error {
-		client, err := grpc_client.NewClient(cc, viper.GetViper())
-		if err != nil {
-			return err
-		}
-		// should be one arg
-		if len(args) != 1 {
-			return fmt.Errorf("deploy requires one argument: database name")
-		}
+func dropCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "drop",
+		Short: "Drops a database",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return common.DialGrpc(cmd.Context(), viper.GetViper(), func(ctx context.Context, cc *grpc.ClientConn) error {
+				client, err := grpc_client.NewClient(cc, viper.GetViper())
+				if err != nil {
+					return err
+				}
+				// should be one arg
+				if len(args) != 1 {
+					return fmt.Errorf("deploy requires one argument: database name")
+				}
 
-		data := &databases.DatabaseIdentifier{
-			Name:  args[0],
-			Owner: client.Config.Address,
-		}
+				data := &databases.DatabaseIdentifier{
+					Name:  args[0],
+					Owner: client.Config.Address,
+				}
 
-		// build tx
-		tx, err := client.BuildTransaction(ctx, transactions.DROP_DATABASE, data, client.Config.PrivateKey)
-		if err != nil {
-			return err
-		}
+				// build tx
+				tx, err := client.BuildTransaction(ctx, transactions.DROP_DATABASE, data, client.Config.PrivateKey)
+				if err != nil {
+					return err
+				}
 
-		res, err := client.Txs.Broadcast(ctx, tx)
-		if err != nil {
-			return err
-		}
+				res, err := client.Txs.Broadcast(ctx, tx)
+				if err != nil {
+					return err
+				}
 
-		display.PrintTxResponse(res)
+				display.PrintTxResponse(res)
 
-		return nil
-	})
+				return nil
+			})
+		},
+	}
+	return cmd
 }
