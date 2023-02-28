6bba01a0b1d567a1d64fd640ed2d7cf5c1733834
brennan lamey <brennanlamey@gmail.com>
2023-02-28 11:01:45 -0600

    fixed error msg

--- pkg/accounts/errors.go
+++ pkg/accounts/errors.go
@@ -3,7 +3,7 @@ package accounts
 import "errors"
 
 var (
-	ErrAccountNotRegistered = errors.New("config not registered")
+	ErrAccountNotRegistered = errors.New("account not registered")
 	ErrInsufficientFunds    = errors.New("insufficient funds")
 	ErrInvalidNonce         = errors.New("invalid nonce")
 )
