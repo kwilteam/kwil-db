4378179175cce9907c67b7caad642f3909e0f099
brennan lamey <brennanlamey@gmail.com>
2023-05-11 17:42:45 -0500

    sql tree grammar and sqlite visitor

--- internal/pkg/sqlite/generate.sh
+++ internal/pkg/sqlite/generate.sh
@@ -3,4 +3,4 @@
 antlr_bin=antlr-4.12.0-complete.jar
 
 alias antlr4='java -Xmx500M -cp "./${antlr_bin}:$CLASSPATH" org.antlr.v4.Tool'
-antlr4 -Dlanguage=Go -no-visitor -package sqlite *.g4
+antlr4 -Dlanguage=Go -visitor -package sqlite *.g4
