9f19c75c1aa77ac4f664366e5654e7c4f05d997e
Charitha Bandi <45089429+charithabandi@users.noreply.github.com>
2023-06-12 14:48:25 -0500

    Dynamic Validator set updates (#95)

--- pkg/grpc/client/v1/validator.go
+++ pkg/grpc/client/v1/validator.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/cometbft/cometbft/crypto/ed25519"
 	txpb "github.com/kwilteam/kwil-db/api/protobuf/tx/v1"
+	kTx "github.com/kwilteam/kwil-db/pkg/tx"
 )
 
 func (c *Client) ApproveValidator(ctx context.Context, pubKey ed25519.PubKey) error {
@@ -20,4 +21,22 @@ func (c *Client) ApproveValidator(ctx context.Context, pubKey ed25519.PubKey) er
 
 	fmt.Printf("Validator %s has been approved, Log: %s\n", pubKey, resp.Log)
 	return nil
+}
+
+func (c *Client) ValidatorJoin(ctx context.Context, tx *kTx.Transaction) (*kTx.Receipt, error) {
+	pbTx := ConvertTx(tx)
+	fmt.Println("Broadcasting ValidatorJoin transaction")
+	resp, err := c.txClient.ValidatorJoin(ctx, &txpb.ValidatorJoinRequest{Tx: pbTx})
+	if err != nil {
+		fmt.Println("TxServiceClient failed to join Validator", err)
+		return nil, fmt.Errorf("TxServiceClient failed to join Validator: %w", err)
+	}
+
+	if resp.Receipt == nil {
+		fmt.Println("TxServiceClient failed to join Validator: receipt is nil")
+		return nil, fmt.Errorf("TxServiceClient failed to join Validator: receipt is nil")
+	}
+
+	txRes := ConvertReceipt(resp.Receipt)
+	return txRes, nil
 }
