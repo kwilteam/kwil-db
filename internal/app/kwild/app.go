d237a093555c8e1fe5f126a309c38ce567960fb5
charithabandi <chbandi302@gmail.com>
2023-06-23 10:26:17 -0500

    Kwild cli - first set

--- internal/app/kwild/app.go
+++ internal/app/kwild/app.go
@@ -2,9 +2,21 @@ package kwild
 
 import (
 	"github.com/kwilteam/kwil-db/internal/app/kwild/cmd"
+	"github.com/spf13/cobra"
 )
 
+var rootCmd = &cobra.Command{
+	Use:               "kwild",
+	Short:             "kwild command line interface",
+	Long:              "kwild allows you to configure Kwild services",
+	SilenceUsage:      true,
+	DisableAutoGenTag: true,
+}
+
 func Execute() error {
-	cmd.RootCmd.SilenceUsage = true
-	return cmd.RootCmd.Execute()
+	rootCmd.AddCommand(
+		cmd.NewStartCmd(),
+		cmd.NewStopCmd(),
+	)
+	return rootCmd.Execute()
 }
