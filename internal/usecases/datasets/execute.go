2f49b6ea919d7e22b390bc6c55627a72d25dfa08
Charitha Bandi <45089429+charithabandi@users.noreply.github.com>
2023-06-27 15:26:52 -0500

    Revert "Toggle gas prices (#114)"
    This reverts commit 29a4a66e10ab4411abd7d85e88c686edb1a0e133.

--- internal/usecases/datasets/execute.go
+++ internal/usecases/datasets/execute.go
@@ -9,23 +9,18 @@ import (
 	"github.com/kwilteam/kwil-db/pkg/tx"
 )
 
-func (u *DatasetUseCase) Execute(action *entity.ExecuteAction) (rec *tx.Receipt, err error) {
-	price := big.NewInt(0)
-
-	if u.gas_enabled {
-		price, err = u.PriceExecute(action)
-		if err != nil {
-			return nil, err
-		}
-	}
-	fmt.Printf("Tx fee: %v  Gas Price: %s\n", action.Tx.Fee, price)
-	err = u.compareAndSpend(action.Tx.Sender, action.Tx.Fee, action.Tx.Nonce, price)
+func (u *DatasetUseCase) Execute(action *entity.ExecuteAction) (*tx.Receipt, error) {
+	price, err := u.PriceExecute(action)
 	if err != nil {
-		fmt.Printf("Compare and spend failed: %v\n", err)
 		return nil, err
 	}
 
 	ds, err := u.engine.GetDataset(action.ExecutionBody.DBID)
+	if err != nil {
+		return nil, err
+	}
+
+	err = u.compareAndSpend(action.Tx.Sender, action.Tx.Fee, action.Tx.Nonce, price)
 	if err != nil {
 		return nil, err
 	}
@@ -38,7 +33,7 @@ func (u *DatasetUseCase) Execute(action *entity.ExecuteAction) (rec *tx.Receipt,
 	if err != nil {
 		return nil, err
 	}
-
+	fmt.Println("cherry exec res", res)
 	bts, err := readQueryResult(res)
 	if err != nil {
 		return nil, err
