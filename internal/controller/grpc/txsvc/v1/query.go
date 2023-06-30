62489f6c602a81022a085308ada593b63050cc75
charithabandi <chbandi302@gmail.com>
2023-06-30 14:27:48 -0500

    Query operations removed from COmetbft

--- internal/controller/grpc/txsvc/v1/query.go
+++ internal/controller/grpc/txsvc/v1/query.go
@@ -2,30 +2,21 @@ package txsvc
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 
-	localClient "github.com/cometbft/cometbft/rpc/client/local"
 	txpb "github.com/kwilteam/kwil-db/api/protobuf/tx/v1"
+	"github.com/kwilteam/kwil-db/internal/entity"
 )
 
 func (s *Service) Query(ctx context.Context, req *txpb.QueryRequest) (*txpb.QueryResponse, error) {
-	bcClient := localClient.New(s.BcNode)
-	req_data, err := json.Marshal(req)
+	bts, err := s.executor.Query(ctx, &entity.DBQuery{
+		DBID:  req.Dbid,
+		Query: req.Query,
+	})
 	if err != nil {
-		return nil, fmt.Errorf("failed to serialize query request data: %w", err)
+		return nil, err
 	}
 
-	res, err := bcClient.ABCIQuery(ctx, "", req_data)
-	if err != nil || res.Response.Code != 0 {
-		return nil, fmt.Errorf("failed to query with error:  %s and response code: %v", err, res.Response.Code)
-	}
-
-	var resp *txpb.QueryResponse
-	err = json.Unmarshal(res.Response.Value, &resp)
-	if err != nil {
-		return nil, fmt.Errorf("failed to deserialize query response: %w", err)
-	}
-
-	return resp, nil
+	return &txpb.QueryResponse{
+		Result: bts,
+	}, nil
 }
