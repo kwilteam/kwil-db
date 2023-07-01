245fbd43401839be0a873c3983c0fbe028a7f5cd
brennan lamey <brennanlamey@gmail.com>
2023-06-30 18:25:35 -0600

    added integration tests

--- pkg/engine/dataset/opts.go
+++ pkg/engine/dataset/opts.go
@@ -6,35 +6,39 @@ type TxOpts struct {
 	Caller string
 }
 
-type engineOptions struct {
-	initializers map[string]Initializer
-	owner        string
-	name         string
-	log          log.Logger
-}
-
-type OpenOpt func(*engineOptions)
+type OpenOpt func(*Dataset)
 
 func WithAvailableExtensions(exts map[string]Initializer) OpenOpt {
-	return func(opts *engineOptions) {
+	return func(opts *Dataset) {
 		opts.initializers = exts
 	}
 }
 
 func OwnedBy(owner string) OpenOpt {
-	return func(opts *engineOptions) {
+	return func(opts *Dataset) {
 		opts.owner = owner
 	}
 }
 
 func Named(name string) OpenOpt {
-	return func(opts *engineOptions) {
+	return func(opts *Dataset) {
 		opts.name = name
 	}
 }
 
 func WithLogger(logger log.Logger) OpenOpt {
-	return func(opts *engineOptions) {
+	return func(opts *Dataset) {
 		opts.log = logger
+	}
+}
+
+// TODO: test that this works
+// OpenWithMissingExtensions will open the dataset,
+// even if the server does not have the correct extensions
+// installed.  This should only be used when failing to have an extension
+// effects the entire server (like startup)
+func OpenWithMissingExtensions() OpenOpt {
+	return func(opts *Dataset) {
+		opts.allowMissingExtensions = true
 	}
 }
