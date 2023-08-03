9fd23a3e61f3dc889cc8ed4b1c020153a1ae0b99
yaiba <4yaiba@gmail.com>
2023-08-03 14:09:07 -0500

    fix: change `call` rpc payload to bytes
    Due to serialization issue, client(sdk) and server may have a different view of the data to be signed.
    Use bytes as payload make sure all parties use same data to sign/verify.

--- internal/entity/execution.go
+++ internal/entity/execution.go
@@ -6,17 +6,12 @@ import (
 
 type ExecuteAction struct {
 	Tx            *tx.Transaction
-	ExecutionBody *ActionExecution
+	ExecutionBody *tx.ExecuteActionPayload
 }
 
-type ActionExecution struct {
-	Action string           `json:"action"`
-	DBID   string           `json:"dbid"`
-	Params []map[string]any `json:"params"`
-}
-
-// ActionCall is a struct that represents the action call
+// CallAction is a struct that represents the action call
 // a call is a read-only action
-type ActionCall struct {
-	Message *tx.SignedMessage[*tx.CallActionPayload]
+type CallAction struct {
+	Message *tx.SignedMessage[tx.JsonPayload]
+	Payload *tx.CallActionPayload
 }
