cca7e8ed152bdab9a0a064b5829769ef93e96b25
Yaiba <4yaiba@gmail.com>
2023-08-10 08:01:02 -0700

    fix: kwild utils subcmd share flags vars (#171)
    * fix: utils subcmds no longer use shared vars for flags among different cmds
    
    * fix: remote disable-gas flag for utils subcmd

--- internal/app/kwild/cmd/utils/validator.go
+++ internal/app/kwild/cmd/utils/validator.go
@@ -15,26 +15,27 @@ import (
 // validator.
 
 func GenValidatorCmd() *cobra.Command {
+	var outputDir string
+
 	validatorCmd := &cobra.Command{
 		Use:     "gen-validator",
 		Aliases: []string{"gen_validator"},
 		Short:   "Generate new validator keypair",
-		Run:     genValidator,
+		Run: func(cmd *cobra.Command, args []string) {
+			pv := privval.GenFilePV("", "")
+			jsbz, err := cmtjson.Marshal(pv)
+			if err != nil {
+				panic(err)
+			}
+			err = os.WriteFile(filepath.Join(outputDir, "config/priv_validator_key.json"), jsbz, 0600)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Printf(`%v`, string(jsbz))
+		},
 	}
-	validatorCmd.Flags().StringVar(&outputDir, "o", "./mytestnet", "directory to store initialization data for the testnet")
-	return validatorCmd
-}
 
-func genValidator(cmd *cobra.Command, args []string) {
-	pv := privval.GenFilePV("", "")
-	jsbz, err := cmtjson.Marshal(pv)
-	if err != nil {
-		panic(err)
-	}
-	err = os.WriteFile(filepath.Join(outputDir, "config/priv_validator_key.json"), jsbz, 0600)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf(`%v`, string(jsbz))
+	validatorCmd.Flags().StringVar(&outputDir, "o", ".testnet", "directory to store initialization data for the testnet")
+	return validatorCmd
 }
