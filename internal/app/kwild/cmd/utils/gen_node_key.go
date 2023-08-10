3e5e5687dd5fd1cff1e696442aebaa837b97f850
Yaiba <4yaiba@gmail.com>
2023-08-10 08:01:02 -0700

    fix: kwild utils subcmd share flags vars (#171)
    * fix: utils subcmds no longer use shared vars for flags among different cmds
    
    * fix: remote disable-gas flag for utils subcmd

--- internal/app/kwild/cmd/utils/gen_node_key.go
+++ internal/app/kwild/cmd/utils/gen_node_key.go
@@ -10,6 +10,10 @@ import (
 	"github.com/cometbft/cometbft/p2p"
 )
 
+var genFlags = struct {
+	homeDir string
+}{}
+
 // GenNodeKeyCmd allows the generation of a node key. It prints node's ID to
 // the standard output.
 func GenNodeKeyCmd() *cobra.Command {
@@ -19,15 +23,16 @@ func GenNodeKeyCmd() *cobra.Command {
 		Short:   "Generate a node key for this node and print its ID",
 		RunE:    genNodeKey,
 	}
-	cmd.Flags().StringVar(&outputDir, "home", "", "comet home directory")
-	if outputDir == "" {
-		outputDir = os.Getenv("COMET_BFT_HOME")
+
+	cmd.Flags().StringVar(&genFlags.homeDir, "home", "", "comet home directory")
+	if genFlags.homeDir == "" {
+		genFlags.homeDir = os.Getenv("COMET_BFT_HOME")
 	}
 	return cmd
 }
 
 func genNodeKey(cmd *cobra.Command, args []string) error {
-	nodeKey, err := p2p.LoadOrGenNodeKey(filepath.Join(outputDir, "config/node_key.json"))
+	nodeKey, err := p2p.LoadOrGenNodeKey(filepath.Join(genFlags.homeDir, "config/node_key.json"))
 	if err != nil {
 		return err
 	}
--- internal/app/kwild/cmd/utils/reset.go
+++ internal/app/kwild/cmd/utils/reset.go
@@ -16,79 +16,94 @@ import (
 var keepAddrBook bool
 
 func NewResetAllCmd() *cobra.Command {
-	ResetAllCmd.Flags().StringVar(&outputDir, "home", "", "comet home directory")
-	if outputDir == "" {
-		outputDir = os.Getenv("COMET_BFT_HOME")
-	}
-	ResetAllCmd.Flags().BoolVar(&keepAddrBook, "keep-addr-book", false, "keep the address book intact")
-	return ResetAllCmd
-}
+	var homeDir string
 
-var ResetAllCmd = &cobra.Command{
-	Use:     "unsafe-reset-all",
-	Aliases: []string{"unsafe_reset_all"},
-	Short:   "(unsafe) Remove all the blockchain's data and WAL, reset this node's validator to genesis state, for testing purposes only",
-	RunE:    resetAllCmd,
+	// XXX: this is totally unsafe.
+	// it's only suitable for testnets.
+	cmd := &cobra.Command{
+		Use:     "unsafe-reset-all",
+		Aliases: []string{"unsafe_reset_all"},
+		Short:   "(unsafe) Remove all the blockchain's data and WAL, reset this node's validator to genesis state, for testing purposes only",
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			config, err := ParseConfig(cmd, homeDir)
+			if err != nil {
+				return err
+			}
+
+			return utils.ResetAll(
+				config.DBDir(),
+				config.P2P.AddrBookFile(),
+				config.PrivValidatorKeyFile(),
+				config.PrivValidatorStateFile(),
+			)
+		},
+	}
+
+	cmd.Flags().StringVar(&homeDir, "home", "", "comet home directory")
+	// TODO: let viper handle this
+	if homeDir == "" {
+		homeDir = os.Getenv("COMET_BFT_HOME")
+	}
+	cmd.Flags().BoolVar(&keepAddrBook, "keep-addr-book", false, "keep the address book intact")
+	return cmd
 }
 
 // ResetStateCmd removes the database of the specified CometBFT core instance.
 func NewResetStateCmd() *cobra.Command {
-	ResetStateCmd.Flags().StringVar(&outputDir, "home", "", "comet home directory")
-	if outputDir == "" {
-		outputDir = os.Getenv("COMET_BFT_HOME")
+	var homeDir string
+
+	cmd := &cobra.Command{
+		Use:     "reset-state",
+		Aliases: []string{"reset_state"},
+		Short:   "(unsafe) Remove all the data and WAL, for testing purposes only",
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			conf, err := ParseConfig(cmd, homeDir)
+			if err != nil {
+				return err
+			}
+
+			return utils.ResetState(conf.DBDir())
+		},
 	}
-	return ResetStateCmd
-}
 
-var ResetStateCmd = &cobra.Command{
-	Use:     "reset-state",
-	Aliases: []string{"reset_state"},
-	Short:   "(unsafe) Remove all the data and WAL, for testing purposes only",
-	RunE: func(cmd *cobra.Command, args []string) (err error) {
-		conf, err := ParseConfig(cmd)
-		if err != nil {
-			return err
-		}
-
-		return utils.ResetState(conf.DBDir())
-	},
+	cmd.Flags().StringVar(&homeDir, "home", "", "comet home directory")
+	if homeDir == "" {
+		homeDir = os.Getenv("COMET_BFT_HOME")
+	}
+	return cmd
 }
 
 // ResetPrivValidatorCmd resets the private validator files.
 func NewResetPrivValidatorCmd() *cobra.Command {
-	ResetPrivValidatorCmd.Flags().StringVar(&outputDir, "home", "", "comet home directory")
-	if outputDir == "" {
-		outputDir = os.Getenv("COMET_BFT_HOME")
-	}
-	return ResetPrivValidatorCmd
-}
+	var homeDir string
 
-var ResetPrivValidatorCmd = &cobra.Command{
-	Use:     "unsafe-reset-priv-validator",
-	Aliases: []string{"unsafe_reset_priv_validator"},
-	Short:   "(unsafe) Reset this node's validator to genesis state, for testing purposes only",
-	RunE:    resetPrivValidator,
-}
+	// XXX: this is totally unsafe.
+	// it's only suitable for testnets.
+	cmd := &cobra.Command{
+		Use:     "unsafe-reset-priv-validator",
+		Aliases: []string{"unsafe_reset_priv_validator"},
+		Short:   "(unsafe) Reset this node's validator to genesis state, for testing purposes only",
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			config, err := ParseConfig(cmd, homeDir)
+			if err != nil {
+				return err
+			}
 
-// XXX: this is totally unsafe.
-// it's only suitable for testnets.
-func resetAllCmd(cmd *cobra.Command, args []string) (err error) {
-	config, err := ParseConfig(cmd)
-	if err != nil {
-		return err
+			utils.ResetFilePV(config.PrivValidatorKeyFile(), config.PrivValidatorStateFile())
+			return nil
+		},
 	}
 
-	return utils.ResetAll(
-		config.DBDir(),
-		config.P2P.AddrBookFile(),
-		config.PrivValidatorKeyFile(),
-		config.PrivValidatorStateFile(),
-	)
+	cmd.Flags().StringVar(&homeDir, "home", "", "comet home directory")
+	if homeDir == "" {
+		homeDir = os.Getenv("COMET_BFT_HOME")
+	}
+	return cmd
 }
 
-func ParseConfig(cmd *cobra.Command) (*cfg.Config, error) {
+func ParseConfig(cmd *cobra.Command, homeDir string) (*cfg.Config, error) {
 	conf := cfg.DefaultConfig()
-	conf.SetRoot(outputDir)
+	conf.SetRoot(homeDir)
 	cfg.EnsureRoot(conf.RootDir)
 
 	if err := conf.ValidateBasic(); err != nil {
@@ -100,16 +115,4 @@ func ParseConfig(cmd *cobra.Command) (*cfg.Config, error) {
 		}
 	}
 	return conf, nil
-}
-
-// XXX: this is totally unsafe.
-// it's only suitable for testnets.
-func resetPrivValidator(cmd *cobra.Command, args []string) (err error) {
-	config, err := ParseConfig(cmd)
-	if err != nil {
-		return err
-	}
-
-	utils.ResetFilePV(config.PrivValidatorKeyFile(), config.PrivValidatorStateFile())
-	return nil
 }
