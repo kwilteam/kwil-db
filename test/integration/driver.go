d2d19d3c136a8ca676141755f8ca0b0cc6464186
Yaiba <4yaiba@gmail.com>
2023-08-14 11:40:26 -0700

    refactor test to run with new setup (#183)

---
+++ test/integration/driver.go
@@ -0,0 +1,11 @@
+package integration
+
+import "github.com/kwilteam/kwil-db/test/specifications"
+
+type KwilIntDriver interface {
+	specifications.DatabaseDeployDsl
+	specifications.DatabaseDropDsl
+	specifications.ExecuteQueryDsl
+	specifications.ExecuteCallDsl
+	specifications.NetworkOpsDsl
+}
