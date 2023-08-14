47e0f8647598c9b6d8588020b54944b7134ff398
brennan lamey <brennanlamey@gmail.com>
2023-08-14 10:13:56 -0500

    removed old packages

--- pkg/_tx/response.go
+++
@@ -1,9 +0,0 @@
-package tx
-
-import "math/big"
-
-// ExecutionResponse is the response from any interaction that modifies state.
-type ExecutionResponse struct {
-	// Fee is the amount of tokens spent on the execution
-	Fee *big.Int
-}
