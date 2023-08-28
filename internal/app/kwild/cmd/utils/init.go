5acb58acb1b8fb5d9ae8de71fd0017d8a000837f
yaiba <4yaiba@gmail.com>
2023-08-28 13:57:17 -0500

    fix: `utils` generate subcmds will not load config

--- internal/app/kwild/cmd/utils/init.go
+++ internal/app/kwild/cmd/utils/init.go
@@ -1,8 +1,6 @@
 package utils
 
 import (
-	"os"
-
 	"github.com/kwilteam/kwil-db/internal/pkg/nodecfg"
 	"github.com/spf13/cobra"
 )
@@ -16,13 +14,11 @@ func InitFilesCmd() *cobra.Command {
 		Short: "Initializes files required for a kwil node",
 		RunE:  initFiles,
 	}
-	initFilesCmd.Flags().StringVar(&initFlags.HomeDir, "home", "", "comet home directory")
-	// TODO: let viper handle this
-	if initFlags.HomeDir == "" {
-		initFlags.HomeDir = os.Getenv("COMET_BFT_HOME")
-	}
-	initFilesCmd.Flags().Int64Var(&initFlags.InitialHeight, "initial-height", 0, "initial height of the first block")
 
+	initFilesCmd.Flags().StringVarP(&initFlags.OutputDir, "output-dir", "o", ".kwild",
+		"directory to store initialization data for the node")
+	initFilesCmd.Flags().Int64VarP(&initFlags.InitialHeight, "initial-height", "i", 0,
+		"initial height of the first block")
 	return initFilesCmd
 }
 
--- internal/app/kwild/cmd/utils/testnet.go
+++ internal/app/kwild/cmd/utils/testnet.go
@@ -28,13 +28,13 @@ func initTestnet(cmd *cobra.Command, args []string) error {
 }
 
 func NewTestnetCmd() *cobra.Command {
-	testnetCmd.Flags().IntVar(&netFlags.NValidators, "v", 4, "number of validators to initialize the testnet with")
+	testnetCmd.Flags().IntVarP(&netFlags.NValidators, "validator-num", "v", 4, "number of validators to initialize the testnet with")
 
-	testnetCmd.Flags().IntVar(&netFlags.NNonValidators, "n", 4, "number of non validators to initialize the testnet with")
+	testnetCmd.Flags().IntVarP(&netFlags.NNonValidators, "non-validator-num", "n", 4, "number of non validators to initialize the testnet with")
 
 	testnetCmd.Flags().StringVar(&netFlags.ConfigFile, "config", "", "config file to use (note some options may be overwritten)")
 
-	testnetCmd.Flags().StringVar(&netFlags.OutputDir, "o", ".testnet", "directory to store initialization data for the testnet")
+	testnetCmd.Flags().StringVarP(&netFlags.OutputDir, "output-dir", "o", ".testnet", "directory to store initialization data for the testnet")
 
 	testnetCmd.Flags().StringVar(&netFlags.NodeDirPrefix, "node-dir-prefix", "node", "prefix the directory name for each node with (node results in node0, node1, ...)")
 
