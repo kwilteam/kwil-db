f81cb653246779e31a3ffd5d8931f93667cf4770
yaiba <4yaiba@gmail.com>
2023-08-30 13:12:45 -0500

    feat: friendly signing msg

--- pkg/crypto/signer_test.go
+++ pkg/crypto/signer_test.go
@@ -2,6 +2,7 @@ package crypto
 
 import (
 	"encoding/hex"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -10,7 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestComebftSecp256k1Signer_SignMsg(t *testing.T) {
+func TestComebftSecp256k1Signer_Sign(t *testing.T) {
 	msg := []byte("foo")
 	pvKeyHex := "f1aa5a7966c3863ccde3047f6a1e266cdc0c76b399e256b8fede92b1c69e4f4e"
 	expectSignatureHex := "19a4aced02d5b9142b4f622b06442b1904445e16bd25409e6b0ff357ccc021d001d0e7824654b695b4b6e0991cb7507f487b82be4b2ed713d1e3e2cbc3d2518d"
@@ -33,7 +34,7 @@ func TestComebftSecp256k1Signer_SignMsg(t *testing.T) {
 	require.NoError(t, err, "error parse private pvKeyHex")
 
 	cometBfgSigner := NewCometbftSecp256k1Signer(kwilCometBftKey)
-	sig, err := cometBfgSigner.SignMsg(msg)
+	sig, err := cometBfgSigner.Sign(msg)
 	assert.NoError(t, err, "error signing message")
 	require.EqualValues(t, expectSig, sig, "signature mismatch")
 
@@ -41,7 +42,7 @@ func TestComebftSecp256k1Signer_SignMsg(t *testing.T) {
 	assert.NoError(t, err, "error verifying signature")
 }
 
-func TestEthPersonalSecp256k1Signer_SignMsg(t *testing.T) {
+func TestEthPersonalSecp256k1Signer_Sign(t *testing.T) {
 	msg := []byte("foo")
 	pvKeyHex := "f1aa5a7966c3863ccde3047f6a1e266cdc0c76b399e256b8fede92b1c69e4f4e"
 	expectSignatureHex := "cb3fed7f6ff36e59054c04a831b215e514052753ee353e6fe31d4b4ef736acd6155127db555d3006ba14fcb4c79bbad56c8e63b81a9896319bb053a9e253475800"
@@ -57,7 +58,7 @@ func TestEthPersonalSecp256k1Signer_SignMsg(t *testing.T) {
 
 	ethSigner := NewEthPersonalSecp256k1Signer(pk)
 
-	sig, err := ethSigner.SignMsg(msg)
+	sig, err := ethSigner.Sign(msg)
 	require.NoError(t, err, "error signing msg")
 	assert.EqualValues(t, expectSig, sig, "signature mismatch")
 
@@ -65,7 +66,7 @@ func TestEthPersonalSecp256k1Signer_SignMsg(t *testing.T) {
 	assert.NoError(t, err, "error verifying signature")
 }
 
-func TestStdEd25519Signer_SignMsg(t *testing.T) {
+func TestStdEd25519Signer_Sign(t *testing.T) {
 	msg := []byte("foo")
 	pvKeyHex := "7c67e60fce0c403ff40193a3128e5f3d8c2139aed36d76d7b5f1e70ec19c43f00aa611bf555596912bc6f9a9f169f8785918e7bab9924001895798ff13f05842"
 	expectSignature := "59b2db2d1e4ce6f8771453cfc78d1f943723528f00fa14adf574600f15c601d591fa2ba29c94d9ed694db324f9e8671bdfbcba4b8e10f6a8733682fa3d115f0c"
@@ -81,7 +82,7 @@ func TestStdEd25519Signer_SignMsg(t *testing.T) {
 
 	edSigner := NewStdEd25519Signer(pk)
 
-	sig, err := edSigner.SignMsg(msg)
+	sig, err := edSigner.Sign(msg)
 	require.NoError(t, err, "error sign")
 	assert.EqualValues(t, expectSig, sig, "signature mismatch")
 
@@ -89,8 +90,33 @@ func TestStdEd25519Signer_SignMsg(t *testing.T) {
 	assert.NoError(t, err, "error verifying signature")
 }
 
-func TestTrivialSigner_SignMsg(t *testing.T) {
+func TestNearEd25519Signer_Sign(t *testing.T) {
+	msg := []byte("foo")
+	pvKeyHex := "7c67e60fce0c403ff40193a3128e5f3d8c2139aed36d76d7b5f1e70ec19c43f00aa611bf555596912bc6f9a9f169f8785918e7bab9924001895798ff13f05842"
+	expectSignature := "089bcf52220dad77abc2cfcb1639bcb2944fdf64e0b173f40cd0d144bdbf7808f4eff3716eb3e98ed40be3ab126e1449d5f57efbe5626673059edc90e9cd9801"
+	expectSignatureBytes, _ := hex.DecodeString(expectSignature)
+	expectSig := &Signature{
+		Signature: expectSignatureBytes,
+		Type:      SignatureTypeEd25519Near,
+	}
+	require.Equal(t, SignatureEd25519Length, len(expectSignatureBytes), "invalid signature length")
+
+	pk, err := Ed25519PrivateKeyFromHex(pvKeyHex)
+	require.NoError(t, err, "error parse private key")
+
+	nearSigner := NewNearSigner(pk)
+
+	sig, err := nearSigner.Sign(msg)
+	require.NoError(t, err, "error sign")
+	assert.EqualValues(t, expectSig, sig, "signature mismatch")
+	fmt.Println(hex.EncodeToString(sig.Signature))
+
+	err = sig.Verify(pk.PubKey(), msg)
+	assert.NoError(t, err, "error verifying signature")
+}
+
+func TestTrivialSigner_Sign(t *testing.T) {
 	signer := NewTrivialSigner(nil)
-	_, err := signer.SignMsg([]byte("foo"))
+	_, err := signer.Sign([]byte("foo"))
 	require.Error(t, err, "suppose to error")
 }
--- pkg/transactions/transaction_test.go
+++ pkg/transactions/transaction_test.go
@@ -1,11 +1,14 @@
 package transactions_test
 
 import (
+	"encoding/hex"
+	"fmt"
 	"math/big"
 	"testing"
 
 	"github.com/kwilteam/kwil-db/pkg/crypto"
 	"github.com/kwilteam/kwil-db/pkg/transactions"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -34,4 +37,183 @@ func Test_TransactionMarshal(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, tx, tx2)
+}
+
+func TestTransaction_Sign(t *testing.T) {
+	// secp256k1
+	secp2561k1PvKeyHex := "f1aa5a7966c3863ccde3047f6a1e266cdc0c76b399e256b8fede92b1c69e4f4e"
+	secp256k1PrivateKey, err := crypto.Secp256k1PrivateKeyFromHex(secp2561k1PvKeyHex)
+	require.NoError(t, err, "error parse private secp2561k1PvKeyHex")
+
+	ethPersonalSigner := crypto.NewEthPersonalSecp256k1Signer(secp256k1PrivateKey)
+
+	expectPersonalSignConcatSigHex := "4a8f9a2eea6fc6b6d055a13603bd9fc9495283a20d12cf44742673fb297a8f7f2b61231eeac778df354f10191562167e86275bebd55dbdfe7d2377b96e09d74901"
+	expectPersonalSignConcatSigBytes, _ := hex.DecodeString(expectPersonalSignConcatSigHex)
+	expectPersonalSignConcatSig := &crypto.Signature{
+		Signature: expectPersonalSignConcatSigBytes,
+		Type:      crypto.SignatureTypeSecp256k1Personal,
+	}
+
+	// TODO: add test case for cometbft
+	//cometbftSigner := crypto.NewCometbftSecp256k1Signer(secp256k1PrivateKey)
+	//expectCometbftConcatSigHex
+
+	//expectEip721SigHex := ""
+	////
+	// ed25519
+	ed25519PvKeyHex := "7c67e60fce0c403ff40193a3128e5f3d8c2139aed36d76d7b5f1e70ec19c43f00aa611bf555596912bc6f9a9f169f8785918e7bab9924001895798ff13f05842"
+	ed25519PrivateKey, err := crypto.Ed25519PrivateKeyFromHex(ed25519PvKeyHex)
+	require.NoError(t, err, "error parse ed25519PvKeyHex")
+
+	nearSigner := crypto.NewNearSigner(ed25519PrivateKey)
+
+	expectNearConcatSigHex := "b72f815bcb5a7126fe54cd8d77210209249b1c11087a4b4601c61814911fac7f4a921f1c37991408251a04891a5e10ecc1be4535124a3827f22660adbec0590c"
+	expectNearConcatSigBytes, _ := hex.DecodeString(expectNearConcatSigHex)
+	expectNearConcatSig := &crypto.Signature{
+		Signature: expectNearConcatSigBytes,
+		Type:      crypto.SignatureTypeEd25519Near,
+	}
+	////
+
+	rawPayload := transactions.ActionExecution{
+		DBID:   "xf617af1ca774ebbd6d23e8fe12c56d41d25a22d81e88f67c6c6ee0d4",
+		Action: "create_user",
+		Arguments: [][]string{
+			{"foo", "32"},
+		},
+	}
+
+	payloadRLP, err := rawPayload.MarshalBinary()
+	require.NoError(t, err)
+
+	type args struct {
+		mst    transactions.SignedMsgSerializationType
+		signer crypto.Signer
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantSig *crypto.Signature
+		wantErr bool
+	}{
+		{
+			name: "not support message serialization type",
+			args: args{
+				mst:    transactions.SignedMsgSerializationType("not support message serialization type"),
+				signer: ethPersonalSigner,
+			},
+			wantErr: true,
+		},
+		{
+			name: "eth personal_sign concat string",
+			args: args{
+				mst:    transactions.SignedMsgConcat,
+				signer: ethPersonalSigner,
+			},
+			wantSig: expectPersonalSignConcatSig,
+		},
+		{
+			name: "near concat string",
+			args: args{
+				mst:    transactions.SignedMsgConcat,
+				signer: nearSigner,
+			},
+			wantSig: expectNearConcatSig,
+		},
+		//{}, // eth eip712
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t1 *testing.T) {
+			tx := transactions.Transaction{
+				Body: &transactions.TransactionBody{
+					Payload:     payloadRLP,
+					PayloadType: rawPayload.Type(),
+					Fee:         big.NewInt(100),
+					Nonce:       1,
+					Salt:        []byte("salt"),
+					Description: "By signing this message, you'll reveal your xxx to zzz",
+				},
+				Serialization: tt.args.mst,
+			}
+
+			err := tx.Sign(tt.args.signer)
+			if tt.wantErr {
+				assert.Error(t1, err, "Sign(%v)", tt.args.mst)
+				return
+			}
+
+			require.NoError(t1, err, "error signing tx")
+			require.Equal(t1, tt.wantSig.Type, tx.Signature.Type,
+				"mismatch signature type")
+			require.Equal(t1, hex.EncodeToString(tt.wantSig.Signature),
+				hex.EncodeToString(tx.Signature.Signature), "mismatch signature")
+
+			err = tx.Verify()
+			require.NoError(t1, err, "error verifying tx")
+		})
+	}
+}
+
+func TestTransactionBody_SerializeMsg(t *testing.T) {
+	rawPayload := transactions.ActionExecution{
+		DBID:   "xf617af1ca774ebbd6d23e8fe12c56d41d25a22d81e88f67c6c6ee0d4",
+		Action: "create_user",
+		Arguments: [][]string{
+			{"foo", "32"},
+		},
+	}
+
+	payload, err := rawPayload.MarshalBinary()
+	require.NoError(t, err)
+
+	txBody := &transactions.TransactionBody{
+		Payload:     payload,
+		PayloadType: rawPayload.Type(),
+		Fee:         big.NewInt(100),
+		Nonce:       1,
+		Salt:        []byte("salt"),
+		Description: "By signing this message, you'll reveal your xxx to zzz",
+	}
+
+	type args struct {
+		mst transactions.SignedMsgSerializationType
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		wantMsg string //hex string
+		wantErr bool
+	}{
+		{
+			name: "non support message serialization type",
+			args: args{
+				mst: transactions.SignedMsgSerializationType("non support message serialization type"),
+			},
+			wantMsg: "",
+			wantErr: true,
+		},
+		{
+			name: "concat string",
+			args: args{
+				mst: transactions.SignedMsgConcat,
+			},
+			wantMsg: "4279207369676e696e672074686973206d6573736167652c20796f75276c6c2072657665616c20796f75722078787820746f207a7a7a0a0a5061796c6f6164547970653a20657865637574655f616374696f6e0a5061796c6f61644469676573743a20386531326432386530313665316139306331386662333037316331316137663038306462383764330a4665653a203130300a4e6f6e63653a20310a53616c743a2037333631366337340a0a4b77696c20f09f968b0a",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t1 *testing.T) {
+			got, err := txBody.SerializeMsg(tt.args.mst)
+			if tt.wantErr {
+				assert.Error(t1, err, "SerializeMsg(%v)", tt.args.mst)
+				return
+			}
+
+			assert.NoError(t1, err, "SerializeMsg(%v)", tt.args.mst)
+			assert.Equalf(t1, tt.wantMsg, hex.EncodeToString(got), "SerializeMsg(%v)", tt.args.mst)
+			fmt.Printf("msg to sign: \n%s\n", string(got))
+
+		})
+	}
 }
