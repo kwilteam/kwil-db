6b1f9ae4efd4af2de64051ed412d458f9c0c266e
charithabandi <chbandi302@gmail.com>
2023-09-12 10:07:17 -0500

    Minor fixes in integration test

--- test/integration/driver.go
+++ test/integration/driver.go
@@ -7,5 +7,5 @@ type KwilIntDriver interface {
 	specifications.DatabaseDropDsl
 	specifications.ExecuteQueryDsl
 	specifications.ExecuteCallDsl
-	specifications.NetworkOpsDsl
+	specifications.ValidatorOpsDsl
 }
