cd11239ebc2c05410a6bdcb05fd005e39c4fa4b9
yaiba <4yaiba@gmail.com>
2023-09-22 09:55:08 -0500

    feat(actparser): extension_call and action_call support sqlparser.Expression as argument

--- pkg/engine/dataset/actparser/parser_test.go
+++ pkg/engine/dataset/actparser/parser_test.go
@@ -4,44 +4,134 @@ import (
 	"flag"
 	"testing"
 
+	"github.com/kwilteam/kwil-db/pkg/engine/sqlparser/tree"
 	"github.com/stretchr/testify/assert"
 )
 
 var trace = flag.Bool("trace", false, "run tests with tracing")
 
 func TestParseActionStmt(t *testing.T) {
-
 	tests := []struct {
 		name   string
 		input  string
 		expect ActionStmt
 	}{
 		{
-			name:  "action_call",
-			input: `action_xx($a, 2, "3");`,
+			name: "action_call",
+			// both `-2` and `- 2` will be parsed as ExpressionUnary
+			input: `action_xx(2, '3', $a, @b, -2, 1 + - 2, (1 * 2) + 3, 1 <= 2, 1 and $c, address($c), address(upper($c)));`,
 			expect: &ActionCallStmt{
 				Method: "action_xx",
-				Args: []string{
-					`$a`,
-					`2`,
-					`"3"`,
+				Args: []tree.Expression{
+					&tree.ExpressionLiteral{Value: "2"},
+					&tree.ExpressionLiteral{Value: `"3"`},
+					&tree.ExpressionBindParameter{Parameter: "$a"},
+					&tree.ExpressionBindParameter{Parameter: "@b"},
+					&tree.ExpressionUnary{
+						Operator: tree.UnaryOperatorMinus,
+						Operand:  &tree.ExpressionLiteral{Value: "2"},
+					},
+					&tree.ExpressionArithmetic{
+						Left:     &tree.ExpressionLiteral{Value: "1"},
+						Operator: tree.ArithmeticOperatorAdd,
+						Right: &tree.ExpressionUnary{
+							Operator: tree.UnaryOperatorMinus,
+							Operand:  &tree.ExpressionLiteral{Value: "2"},
+						},
+					},
+					&tree.ExpressionArithmetic{
+						Left: &tree.ExpressionArithmetic{
+							Wrapped:  true,
+							Left:     &tree.ExpressionLiteral{Value: "1"},
+							Operator: tree.ArithmeticOperatorMultiply,
+							Right:    &tree.ExpressionLiteral{Value: "2"},
+						},
+						Operator: tree.ArithmeticOperatorAdd,
+						Right:    &tree.ExpressionLiteral{Value: "3"},
+					},
+					&tree.ExpressionBinaryComparison{
+						Left:     &tree.ExpressionLiteral{Value: "1"},
+						Operator: tree.ComparisonOperatorLessThanOrEqual,
+						Right:    &tree.ExpressionLiteral{Value: "2"},
+					},
+					&tree.ExpressionBinaryComparison{
+						Left:     &tree.ExpressionLiteral{Value: "1"},
+						Operator: tree.LogicalOperatorAnd,
+						Right:    &tree.ExpressionBindParameter{Parameter: "$c"},
+					},
+					&tree.ExpressionFunction{
+						Function: &tree.FunctionAddress,
+						Inputs:   []tree.Expression{&tree.ExpressionBindParameter{Parameter: "$c"}},
+					},
+					&tree.ExpressionFunction{
+						Function: &tree.FunctionAddress,
+						Inputs: []tree.Expression{
+							&tree.ExpressionFunction{
+								Function: &tree.FunctionUPPER,
+								Inputs:   []tree.Expression{&tree.ExpressionBindParameter{Parameter: "$c"}},
+							},
+						},
+					},
 				},
 			},
 		},
 		{
 			name:  "extension_call",
-			input: `$a, $b = erc20.transfer($q, 2, "3");`,
+			input: `$a, $b = erc20.transfer(2, '3', $a, @b, -2, 1 + - 2, (1 * 2) + 3, 1 <= 2, 1 and $c, address($c), address(upper($c)));`,
 			expect: &ExtensionCallStmt{
 				Extension: "erc20",
 				Method:    "transfer",
-				Args: []string{
-					`$q`,
-					`2`,
-					`"3"`,
-				},
-				Receivers: []string{
-					`$a`,
-					`$b`,
+				Receivers: []string{"$a", "$b"},
+				Args: []tree.Expression{
+					&tree.ExpressionLiteral{Value: "2"},
+					&tree.ExpressionLiteral{Value: `"3"`},
+					&tree.ExpressionBindParameter{Parameter: "$a"},
+					&tree.ExpressionBindParameter{Parameter: "@b"},
+					&tree.ExpressionUnary{
+						Operator: tree.UnaryOperatorMinus,
+						Operand:  &tree.ExpressionLiteral{Value: "2"},
+					},
+					&tree.ExpressionArithmetic{
+						Left:     &tree.ExpressionLiteral{Value: "1"},
+						Operator: tree.ArithmeticOperatorAdd,
+						Right: &tree.ExpressionUnary{
+							Operator: tree.UnaryOperatorMinus,
+							Operand:  &tree.ExpressionLiteral{Value: "2"},
+						},
+					},
+					&tree.ExpressionArithmetic{
+						Left: &tree.ExpressionArithmetic{
+							Wrapped:  true,
+							Left:     &tree.ExpressionLiteral{Value: "1"},
+							Operator: tree.ArithmeticOperatorMultiply,
+							Right:    &tree.ExpressionLiteral{Value: "2"},
+						},
+						Operator: tree.ArithmeticOperatorAdd,
+						Right:    &tree.ExpressionLiteral{Value: "3"},
+					},
+					&tree.ExpressionBinaryComparison{
+						Left:     &tree.ExpressionLiteral{Value: "1"},
+						Operator: tree.ComparisonOperatorLessThanOrEqual,
+						Right:    &tree.ExpressionLiteral{Value: "2"},
+					},
+					&tree.ExpressionBinaryComparison{
+						Left:     &tree.ExpressionLiteral{Value: "1"},
+						Operator: tree.LogicalOperatorAnd,
+						Right:    &tree.ExpressionBindParameter{Parameter: "$c"},
+					},
+					&tree.ExpressionFunction{
+						Function: &tree.FunctionAddress,
+						Inputs:   []tree.Expression{&tree.ExpressionBindParameter{Parameter: "$c"}},
+					},
+					&tree.ExpressionFunction{
+						Function: &tree.FunctionAddress,
+						Inputs: []tree.Expression{
+							&tree.ExpressionFunction{
+								Function: &tree.FunctionUPPER,
+								Inputs:   []tree.Expression{&tree.ExpressionBindParameter{Parameter: "$c"}},
+							},
+						},
+					},
 				},
 			},
 		},
@@ -90,6 +180,51 @@ func TestParseActionStmt(t *testing.T) {
 			}
 
 			assert.EqualValues(t, tt.expect, gotAst, "ParseRawSQL() got %+v, want %+v", gotAst, tt.expect)
+		})
+	}
+}
+
+func TestParseActionStmt_scalar_function(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   string
+		wantErr bool
+	}{
+		{
+			name:    "scalar function notexist",
+			input:   `a(notexist($a));`,
+			wantErr: true,
+		},
+	}
+
+	fns := []string{
+		"unicode", "min", "coalesce", "instr", "nullif", "replace", "sign", "ltrim",
+		"substr", "time", "format", "trim", "unhex", "unixepoch", "count", "hex",
+		"lower", "quote", "rtrim", "strftime", "datetime", "max", "ifnull", "like",
+		"upper", "address", "date", "abs", "error", "public_key", "iif", "length",
+		"glob", "typeof", "group_concat",
+	}
+
+	// existing scalar functions
+	for _, fn := range fns {
+		tests = append(tests, struct {
+			name    string
+			input   string
+			wantErr bool
+		}{
+			name:  "scalar function " + fn,
+			input: "a(" + fn + "($a));",
+		})
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := ParseActionStmt(tt.input, nil, *trace)
+			if tt.wantErr {
+				assert.Error(t, err, "ParseActionStmt(%v)", tt.input)
+			} else {
+				assert.NoError(t, err, "ParseActionStmt(%v)", tt.input)
+			}
 		})
 	}
 }
--- pkg/engine/dataset/actparser/statement.go
+++ pkg/engine/dataset/actparser/statement.go
@@ -1,29 +1,26 @@
 package actparser
 
+import "github.com/kwilteam/kwil-db/pkg/engine/sqlparser/tree"
+
 type ActionStmt interface {
 	StmtType() string
 }
 
-//type CallStmt struct {
-//	Method    string
-//	Args      []string
-//	Receivers []string
-//}
-
 type ExtensionCallStmt struct {
 	Extension string
 	Method    string
-	Args      []string
+	Args      []tree.Expression
 	Receivers []string
 }
 
 type ActionCallStmt struct {
-	Database  string
+	Database  string // for future use, e.g. call an action from another kuneiform
 	Method    string
-	Args      []string
+	Args      []tree.Expression
 	Receivers []string
 }
 
+// DMLStmt is a DML statement, we leave the parsing to sqlparser
 type DMLStmt struct {
 	Statement string
 }
