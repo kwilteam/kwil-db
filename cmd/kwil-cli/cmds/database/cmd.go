aa9ae3c4405d17ff9b03a712b3432f73d0872d9b
Jonathan Chappelow <jon@kwil.com>
2023-10-04 12:19:09 -0500

    kwil-cli,pkg/client: add --nonce override flag to database cmds
    This adds the ability to set a nonce when making a transaction.
    
    Rather than giving every method a nonce input arg, I've added a
    WithNonceFunc option to pkg/client.Client. This is perhaps a
    little strange but the cascade from a new argument was rather
    large, and it seemed a little out of place as the other arguments
    pertain to the action rather than the transaction containing it.
    Can be switched if we'd prefer.

--- cmd/kwil-cli/cmds/database/cmd.go
+++ cmd/kwil-cli/cmds/database/cmd.go
@@ -5,25 +5,40 @@ import (
 )
 
 var (
-	rootCmd = &cobra.Command{
+	dbCmd = &cobra.Command{
 		Use:     "database",
 		Aliases: []string{"db"},
 		Short:   "manage databases",
 		Long:    "Database is a command that contains subcommands for interacting with databases",
 	}
+
+	nonceOverride int64
 )
 
 func NewCmdDatabase() *cobra.Command {
-	rootCmd.AddCommand(
+	// readOnlyCmds do not create a transaction.
+	readOnlyCmds := []*cobra.Command{
+		listCmd(),
+		readSchemaCmd(),
+		queryCmd(),
+		callCmd(), // no tx, but may required key for signature, for now
+	}
+	dbCmd.AddCommand(readOnlyCmds...)
+
+	// writeCmds create a transactions, requiring a private key for signing/
+	writeCmds := []*cobra.Command{
 		deployCmd(),
 		dropCmd(),
-		readSchemaCmd(),
 		executeCmd(),
-		listCmd(),
 		batchCmd(),
-		queryCmd(),
-		callCmd(),
-	)
+	}
+	dbCmd.AddCommand(writeCmds...)
 
-	return rootCmd
+	// The write commands may also specify a nonce to use instead of asking the
+	// node for the latest confirmed nonce.
+	for _, cmd := range writeCmds {
+		cmd.Flags().Int64VarP(&nonceOverride, "nonce", "N", -1, "nonce override (-1 means request from server)")
+	}
+
+	return dbCmd
 }
