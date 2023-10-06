5e5c67eba52e29e7770e0ab4b19e186ddebda19f
jchappelow <140431406+jchappelow@users.noreply.github.com>
2023-10-06 12:51:53 -0500

    abci: nonce-ordering of transactions in proposed block (#334)
    * remove hash from Transaction
    
    * abci: nonce-ordering of transactions in proposed block
    
    This reorders transaction in a block proposal as needed to ensure
    transactions from a given sender are in ascending nonce order.
    
    Transcations from different senders are never swapped.
    
    Also add comments on checktx and mempool module.

--- pkg/serialize/prefix.go
+++ pkg/serialize/prefix.go
@@ -1,25 +1,20 @@
 package serialize
 
 import (
-	"bytes"
 	"encoding/binary"
 	"fmt"
 )
 
 // addSerializedTypePrefix adds a prefix to the encoded value to indicate the encoding type.
-func addSerializedTypePrefix(encoding encodingType, encodedValue []byte) (SerializedData, error) {
-	result, err := uint16ToBytes(uint16(encoding))
-	if err != nil {
-		return nil, err
-	}
-
-	return append(result, encodedValue...), nil
+func addSerializedTypePrefix(encoding encodingType, encodedValue []byte) SerializedData {
+	encodingTypeBytes := uint16ToBytes(uint16(encoding))
+	return append(encodingTypeBytes, encodedValue...)
 }
 
 // removeSerializedTypePrefix removes the prefix from the encoded value.
 func removeSerializedTypePrefix(data SerializedData) (encodingType, []byte, error) {
-	if len(data) == 0 {
-		return encodingTypeInvalid, nil, fmt.Errorf("cannot deserialize encoded value: data is empty")
+	if len(data) < 3 {
+		return encodingTypeInvalid, nil, fmt.Errorf("cannot deserialize encoded value: data is too short")
 	}
 	typ, err := bytesToUint16(data[:2])
 	if err != nil {
@@ -29,27 +24,17 @@ func removeSerializedTypePrefix(data SerializedData) (encodingType, []byte, erro
 	return encodingType(typ), data[2:], nil
 }
 
-// uint16ToBytes converts a uint16 to a byte slice.
-func uint16ToBytes(n uint16) ([]byte, error) {
-	buf := new(bytes.Buffer)
-	err := binary.Write(buf, binary.BigEndian, n)
-	if err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
+// uint16ToBytes converts a uint16 to a byte slice (big endian).
+func uint16ToBytes(n uint16) []byte {
+	b := make([]byte, 2)
+	binary.BigEndian.PutUint16(b, n)
+	return b
 }
 
 // bytesToUint16 converts a byte slice to a uint16.
 func bytesToUint16(b []byte) (uint16, error) {
-	if len(b) < 2 {
-		return 0, fmt.Errorf("cannot convert bytes to uint16: bytes are too short")
+	if len(b) != 2 {
+		return 0, fmt.Errorf("cannot convert bytes to uint16: incorrect length")
 	}
-
-	buf := bytes.NewReader(b)
-	var n uint16
-	err := binary.Read(buf, binary.BigEndian, &n)
-	if err != nil {
-		return 0, err
-	}
-	return n, nil
+	return binary.BigEndian.Uint16(b), nil
 }
