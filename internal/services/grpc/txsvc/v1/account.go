1d5a0bd363d4175b5d78ca8cdbeb0e3c9d5f9bb1
Jonathan Chappelow <jon@kwil.com>
2023-10-18 10:57:09 -0500

    txsvc: access chain app for mempool, add status arg to account endpoint
    This gives the account RPC an optional "status" argument so either the
    latest confirmed or pending (unconfirmed) account information.
    
    Also, this enable re-check in abci since we want to evict transactions that
    might have slipped in with a too-low nonce.

--- internal/services/grpc/txsvc/v1/account.go
+++ internal/services/grpc/txsvc/v1/account.go
@@ -7,16 +7,36 @@ import (
 )
 
 func (s *Service) GetAccount(ctx context.Context, req *txpb.GetAccountRequest) (*txpb.GetAccountResponse, error) {
-	acc, err := s.accountStore.GetAccount(ctx, req.PublicKey)
+	// Status is presently just 0 for confirmed and 1 for pending, but there may
+	// be others such as finalized and safe.
+	if req.Status != nil && *req.Status > 0 {
+		// Ask the node application for account info, which includes any unconfirmed.
+		balance, nonce, err := s.nodeApp.AccountInfo(ctx, req.PublicKey)
+		if err != nil {
+			return nil, err
+		}
+		var pubkey []byte
+		if nonce > 0 { // return nil pubkey for non-existent account
+			pubkey = req.PublicKey
+		}
+		return &txpb.GetAccountResponse{
+			Account: &txpb.Account{
+				PublicKey: pubkey, // nil for non-existent account
+				Nonce:     nonce,
+				Balance:   balance.String(),
+			},
+		}, nil
+	}
+
+	acct, err := s.accountStore.GetAccount(ctx, req.PublicKey)
 	if err != nil {
 		return nil, err
 	}
-
 	return &txpb.GetAccountResponse{
 		Account: &txpb.Account{
-			PublicKey: acc.PublicKey, // nil for non-existent account
-			Nonce:     acc.Nonce,
-			Balance:   acc.Balance.String(),
+			PublicKey: acct.PublicKey, // nil for non-existent account
+			Nonce:     acct.Nonce,
+			Balance:   acct.Balance.String(),
 		},
 	}, nil
 }
