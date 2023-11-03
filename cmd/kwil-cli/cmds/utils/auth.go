1e5a26f9ac2d29ff4f381e1ee72ed5eb97a6cf0c
Yaiba <4yaiba@gmail.com>
2023-11-03 17:45:50 -0500

    client compose message using parameter

--- cmd/kwil-cli/cmds/utils/auth.go
+++ cmd/kwil-cli/cmds/utils/auth.go
@@ -60,17 +60,30 @@ func authCmd() *cobra.Command {
 			// KGW auth is not part of Kwil API, we use a standard http client
 			// this client is to reuse connection
 			hc := httpRPC.DefaultHTTPClient()
-			msg, err := requestForAuthentication(hc, conf.GrpcURL, userAddress)
+
+			authURI, err := url.JoinPath(conf.GrpcURL, kgwAuthEndpoint)
+			if err != nil {
+				panic(err)
+			}
+
+			authParam, err := requestAuthParameter(hc, authURI, userAddress)
 			if err != nil {
 				return fmt.Errorf("request for authentication: %w", err)
 			}
+
+			// TODO: need a authVersion configuration, or should this be part of the authParam?
+			// It seems reasonable to have this as part of the authParam, and SDK
+			// will use the version to compose the message using different template.
+			// According to SIWE, the version is fixed to 1 right now.
+			msg := composeAuthMessage(authParam, conf.GrpcURL, userAddress,
+				authURI, "1", conf.ChainID)
 
 			sig, err := promptSigning(&signer, msg)
 			if err != nil {
 				return fmt.Errorf("prompt signing: %w", err)
 			}
 
-			token, err := requestForToken(hc, conf.GrpcURL, userPubkey, sig)
+			token, err := requestAuthToken(hc, conf.GrpcURL, userPubkey, sig)
 			if err != nil {
 				return fmt.Errorf("request for token: %w", err)
 			}
@@ -91,9 +104,9 @@ func authCmd() *cobra.Command {
 	return cmd
 }
 
-// requestForAuthentication requests authentication message from the KGW provider.
+// requestAuthParameter requests authentication parameters from the KGW provider.
 // This will send a GET request to KGW provider, and the provider will return
-// a message that the user needs to check and sign.
+// parameters that the user could use to compose a message to sign.
 // NOTE: this workflow will change to the same workflow in web browser, for now
 // we just sign whatever the provider returns for quick prototyping.
 // The ideal workflow will be:
@@ -101,43 +114,55 @@ func authCmd() *cobra.Command {
 // 2. KGW provider returns a nonce to cli
 // 3. cli composes a message with the nonce and sign it, send to KGW provider
 // 4. KGW provider verifies the signature and returns a token
-func requestForAuthentication(hc *http.Client, target string, address string) (string, error) {
-	targetURL, err := url.Parse(target)
-	if err != nil {
-		return "", fmt.Errorf("parse target: %w", err)
-	}
-
+func requestAuthParameter(hc *http.Client, target string, address string) (*authParam, error) {
 	params := url.Values{
 		"from": []string{address},
 	}
 
-	authURL, err := targetURL.Parse(kgwAuthEndpoint + "?" + params.Encode())
+	req, err := http.NewRequest(http.MethodGet, target+"?"+params.Encode(), nil)
 	if err != nil {
-		return "", fmt.Errorf("parse authURL: %w", err)
-	}
-
-	req, err := http.NewRequest(http.MethodGet, authURL.String(), nil)
-	if err != nil {
-		return "", fmt.Errorf("create request: %w", err)
+		return nil, fmt.Errorf("create request: %w", err)
 	}
 
 	resp, err := hc.Do(req)
 	if err != nil {
-		return "", fmt.Errorf("do request: %w", err)
+		return nil, fmt.Errorf("do request: %w", err)
 	}
 	defer resp.Body.Close()
 
-	var r authResponse
+	var r authGetResponse
 	err = json.NewDecoder(resp.Body).Decode(&r)
 	if err != nil {
-		return "", fmt.Errorf("unmarshal response: %w", err)
+		return nil, fmt.Errorf("unmarshal response: %w", err)
 	}
 
 	if r.Error != "" {
-		return "", fmt.Errorf("got error response: %s", r.Error)
+		return nil, fmt.Errorf("got error response: %s", r.Error)
 	}
 
 	return r.Result, nil
+}
+
+// composeAuthMessage composes the SIWE-like message to sign.
+// param is the result of GET request for authentication.
+// ALl the other parameters are expected from config.
+func composeAuthMessage(param *authParam, domain string, address string, uri string, version string, chainID string) string {
+	var msg bytes.Buffer
+	msg.WriteString(
+		fmt.Sprintf("%s wants you to sign in with your account:\n", domain))
+	msg.WriteString(fmt.Sprintf("%s\n", address))
+	msg.WriteString("\n")
+	if param.Statement != "" {
+		msg.WriteString(fmt.Sprintf("%s\n", param.Statement))
+	}
+	msg.WriteString("\n")
+	msg.WriteString(fmt.Sprintf("URI: %s\n", uri))
+	msg.WriteString(fmt.Sprintf("Version: %s\n", version))
+	msg.WriteString(fmt.Sprintf("Chain ID: %s\n", chainID))
+	msg.WriteString(fmt.Sprintf("Nonce: %s\n", param.Nonce))
+	msg.WriteString(fmt.Sprintf("Issue At: %s\n", param.IssueAt))
+	msg.WriteString(fmt.Sprintf("Expiration Time: %s\n", param.ExpirationTime))
+	return msg.String()
 }
 
 // promptSigning prompts the user to sign a message. User should be aware of
@@ -159,19 +184,36 @@ func promptSigning(signer auth.Signer, msg string) (*auth.Signature, error) {
 	return signer.Sign([]byte(msg))
 }
 
-type authResponse struct {
+// authParam defines the result of GET request for authentication.
+// It's the parameters that will be used to compose the message(SIWE like)
+// to sign.
+type authParam struct {
+	Nonce          string `json:"nonce"`
+	Statement      string `json:"statement"` // optional
+	IssueAt        string `json:"issue_at"`
+	ExpirationTime string `json:"expiration_time"`
+}
+
+type authGetResponse struct {
+	Result *authParam `json:"result"`
+	Error  string     `json:"error"`
+}
+
+type authPostResponse struct {
 	Result string `json:"result"`
 	Error  string `json:"error"`
 }
 
+// authPostPayload defines the payload of POST request for authentication
 type authPostPayload struct {
 	Sender    []byte          `json:"sender"` // sender public key
 	Signature *auth.Signature `json:"signature"`
 }
 
-// requestForToken requests a token from the KGW provider
-// This will send a POST request to the KGW provider, and the provider will return a token in cookie header
-func requestForToken(hc *http.Client, target string, sender []byte,
+// requestAuthToken requests a authenticated token from the KGW provider.
+// This will send a POST request to the KGW provider, and the provider will
+// return a cookie.
+func requestAuthToken(hc *http.Client, target string, sender []byte,
 	sig *auth.Signature) (*http.Cookie, error) {
 	targetURL, err := url.Parse(target)
 	if err != nil {
@@ -205,7 +247,7 @@ func requestForToken(hc *http.Client, target string, sender []byte,
 	}
 	defer resp.Body.Close()
 
-	var r authResponse
+	var r authPostResponse
 	err = json.NewDecoder(resp.Body).Decode(&r)
 	if err != nil {
 		return nil, fmt.Errorf("unmarshal response: %w", err)
