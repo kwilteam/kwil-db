6f777e04c2ec7f7a91aff6ef9dd3e57c05cc8299
Yaiba <4yaiba@gmail.com>
2023-11-22 12:16:10 -0600

    update test kuneiform file and call action spec

--- test/specifications/execute_call_action.go
+++ test/specifications/execute_call_action.go
@@ -2,13 +2,12 @@ package specifications
 
 import (
 	"context"
-	"testing"
-
 	"github.com/cstockton/go-conv"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
-func ExecuteCallSpecification(ctx context.Context, t *testing.T, caller ExecuteCallDsl) {
+func ExecuteCallSpecification(ctx context.Context, t *testing.T, caller ExecuteCallDsl, visitor ExecuteCallDsl) {
 	t.Logf("Executing ExecuteCallSpecification")
 
 	db := SchemaLoader.Load(t, SchemaTestDB)
@@ -33,14 +32,14 @@ func ExecuteCallSpecification(ctx context.Context, t *testing.T, caller ExecuteC
 	_, err = caller.Call(ctx, dbID, "delete_user", nil, false)
 	assert.Error(t, err, "expected error calling mutable action")
 
-	// test that modifiers "public owner view" enforces authentication
-	// the caller here is the correct owner, but not authenticated
+	// test that modifiers "public owner view" enforces checks on the caller
+	// the caller here is the correct owner
 	_, err = caller.Call(ctx, dbID, "owner_only", nil, false)
-	assert.Error(t, err, "expected error calling owner only action without authentication")
+	assert.NoError(t, err, "calling owner only action with owner as sender should succeed")
 
 	// and test that authenticating works
-	_, err = caller.Call(ctx, dbID, "owner_only", nil, true)
-	assert.NoError(t, err, "calling owner only action with authentication should succeed")
+	_, err = visitor.Call(ctx, dbID, "owner_only", nil, false)
+	assert.Error(t, err, "calling owner only action with non-owner as sender should fail")
 }
 
 func checkGetPostResults(t *testing.T, results []map[string]any) {
