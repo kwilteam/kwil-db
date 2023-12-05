21a51106f17003433740d1a8ebf2dbcd8807fb24
Charitha Bandi <45089429+charithabandi@users.noreply.github.com>
2023-12-05 23:07:41 +0530

    internal/sessions: fix duplicate idempotent key

--- internal/sessions/kv.go
+++ internal/sessions/kv.go
@@ -15,11 +15,6 @@ func setCurrentKey(kv KV, idempotencyKey []byte) error {
 	return kv.Set(currentKeyKey, idempotencyKey)
 }
 
-// deleteCurrentKey deletes the current key in the given connection.
-func deleteCurrentKey(kv KV) error {
-	return kv.Delete(currentKeyKey)
-}
-
 // getCurrentKey gets the current key in the given connection.
 // it can return nil if there is no current key.
 func getCurrentKey(kv KV) ([]byte, error) {
