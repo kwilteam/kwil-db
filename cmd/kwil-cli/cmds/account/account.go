6509ce63122b2a9e9f2ed89fd89a72a5c6a79c2a
jchappelow <140431406+jchappelow@users.noreply.github.com>
2023-12-14 02:53:11 -0600

    cli: add `account id` subcommand to return identifier/address (#429)

--- cmd/kwil-cli/cmds/account/account.go
+++ cmd/kwil-cli/cmds/account/account.go
@@ -1,8 +1,34 @@
 package account
 
 import (
+	"encoding/hex"
+	"errors"
+
+	"github.com/kwilteam/kwil-db/cmd/common/display"
+	"github.com/kwilteam/kwil-db/cmd/kwil-cli/config"
+	"github.com/kwilteam/kwil-db/core/crypto/auth"
+
 	"github.com/spf13/cobra"
 )
+
+var idCmd = &cobra.Command{
+	Use:   "id",
+	Short: "Show the account ID.",
+	Long:  "Returns the Kwil account identifier (typically an Ethereum address), if a private key is configured.",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		conf, err := config.LoadCliConfig()
+		if err != nil {
+			return display.PrintErr(cmd, err)
+		}
+
+		if conf.PrivateKey == nil {
+			return display.PrintErr(cmd, errors.New("no private key configured"))
+		}
+
+		signer := &auth.EthPersonalSigner{Key: *conf.PrivateKey}
+		return display.PrintCmd(cmd, display.RespString(hex.EncodeToString(signer.Identity())))
+	},
+}
 
 var nonceOverride int64
 
@@ -16,6 +42,7 @@ func NewCmdAccount() *cobra.Command {
 	trCmd := transferCmd() // gets the nonce override flag
 
 	cmd.AddCommand(
+		idCmd,
 		balanceCmd(),
 		trCmd,
 	)
