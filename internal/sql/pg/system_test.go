9aab81c6adc2c9795dea9dc6dc4efb5a9c0c88fc
jchappelow <140431406+jchappelow@users.noreply.github.com>
2024-02-13 10:12:26 -0600

    pg: tests for system settings checks (#501)

--- internal/sql/pg/system_test.go
+++ internal/sql/pg/system_test.go
@@ -1,6 +1,8 @@
 package pg
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_validateVersion(t *testing.T) {
 	const needMajor, needMinor = 16, 1
@@ -58,6 +60,162 @@ func Test_validateVersion(t *testing.T) {
 			}
 			if gotOk != tt.wantOk {
 				t.Errorf("validateVersion() gotOk = %v, want %v", gotOk, tt.wantOk)
+			}
+		})
+	}
+}
+
+func Test_wantStringFn(t *testing.T) {
+	tests := []struct {
+		name    string
+		fn      settingValidFn
+		check   string
+		wantErr bool
+	}{
+		{
+			"ok equal",
+			wantStringFn("beef"),
+			"beef",
+			false,
+		},
+		{
+			"ok equal",
+			wantStringFn("beef"),
+			"chicken",
+			true,
+		},
+		{
+			"ok no case",
+			wantStringFn("beef"),
+			"BEEf",
+			false,
+		},
+		{
+			"ok space prefix suffix on both",
+			wantStringFn(" beef"),
+			"  beef ",
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.fn(tt.check)
+			if gotErr := err != nil; tt.wantErr != gotErr {
+				t.Errorf("want err %v, got %v", tt.wantErr, gotErr)
+			}
+		})
+	}
+}
+
+func Test_wantOnFn(t *testing.T) {
+	tests := []struct {
+		name    string
+		fn      settingValidFn
+		check   string
+		wantErr bool
+	}{
+		{
+			"ok on",
+			wantOnFn(true),
+			"on",
+			false,
+		},
+		{
+			"ok off",
+			wantOnFn(false),
+			"off",
+			false,
+		},
+		{
+			"not ok on",
+			wantOnFn(false),
+			"on",
+			true,
+		},
+		{
+			"not ok off",
+			wantOnFn(true),
+			"off",
+			true,
+		},
+		{
+			"ok no case or space",
+			wantOnFn(true),
+			" On	",
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.fn(tt.check)
+			if gotErr := err != nil; tt.wantErr != gotErr {
+				t.Errorf("want err %v, got %v", tt.wantErr, gotErr)
+			}
+		})
+	}
+}
+
+func Test_wantMinIntFn(t *testing.T) {
+	tests := []struct {
+		name    string
+		fn      settingValidFn
+		check   string
+		wantErr bool
+	}{
+		{
+			"ok equal",
+			wantMinIntFn(1),
+			"1",
+			false,
+		},
+		{
+			"ok more",
+			wantMinIntFn(1),
+			"2",
+			false,
+		},
+		{
+			"not ok less",
+			wantMinIntFn(1),
+			"0",
+			true,
+		},
+		{
+			"ok negative want and val",
+			wantMinIntFn(-2),
+			"-1",
+			false,
+		},
+		{
+			"not ok negative val",
+			wantMinIntFn(1),
+			"-1",
+			true,
+		},
+		{
+			"ok negative want",
+			wantMinIntFn(-1),
+			"1",
+			false,
+		},
+		{
+			"float not an integer",
+			wantMinIntFn(1),
+			"2.2",
+			true,
+		},
+		{
+			"text not an integer",
+			wantMinIntFn(1),
+			"nope",
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.fn(tt.check)
+			if gotErr := err != nil; tt.wantErr != gotErr {
+				t.Errorf("want err %v, got %v", tt.wantErr, gotErr)
 			}
 		})
 	}
