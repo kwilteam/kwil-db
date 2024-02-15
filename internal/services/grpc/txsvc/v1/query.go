47ccd8b61164e00c8aa13fe3d65d84deeaf32e06
jchappelow <140431406+jchappelow@users.noreply.github.com>
2024-02-15 09:21:43 -0600

    core/rpc/client/user/http: read and interpret error messages
    The request error handling in core/rpc/client/user/http was
    throwing out most error messages sent by the remote service.
    
    This adds uniform error parsing to each of the http client methods.
    
    On the server side, this updates (*Service).Query so it doesn't
    result in "unknown" status codes resulting in http responses with
    500 status codes.

--- internal/services/grpc/txsvc/v1/query.go
+++ internal/services/grpc/txsvc/v1/query.go
@@ -5,17 +5,22 @@ import (
 	"encoding/json"
 
 	txpb "github.com/kwilteam/kwil-db/core/rpc/protobuf/tx/v1"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *Service) Query(ctx context.Context, req *txpb.QueryRequest) (*txpb.QueryResponse, error) {
 	result, err := s.engine.Query(ctx, req.Dbid, req.Query)
 	if err != nil {
-		return nil, err
+		// We don't know for sure that it's an invalid argument, but an invalid
+		// user-provided query isn't an internal server error.
+		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
 	bts, err := json.Marshal(result)
 	if err != nil {
-		return nil, err
+		return nil, status.Errorf(codes.Internal, "failed to marshal call result")
 	}
 
 	return &txpb.QueryResponse{
