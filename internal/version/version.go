d20e5b455b2d060c247f92c142f581711281284b
Jonathan Chappelow <jon@kwil.com>
2024-06-07 18:00:01 -0500

    version: report version 0.8.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -15,7 +15,7 @@ import (
 //   - 0.6.0+release
 //   - 0.6.1
 //   - 0.6.2-alpha0+go1.21.nocgo
-const kwilVersion = "0.8.0-pre"
+const kwilVersion = "0.8.0+release"
 
 // KwildVersion may be set at compile time by:
 //
