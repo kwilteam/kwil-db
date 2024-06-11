213addc62ba384fbb912d8618ac83c0c46fe0995
Jonathan Chappelow <jon@kwil.com>
2024-06-11 13:25:09 -0500

    statesync: never return UNKNOWN error for apply snap response

--- internal/statesync/codes.go
+++ internal/statesync/codes.go
@@ -19,38 +19,57 @@ var (
 	ErrAbortSnapshotChunk   = fmt.Errorf("abort snapshot chunk")
 	ErrRetrySnapshotChunk   = fmt.Errorf("retry snapshot chunk")   // retries without refetching the chunk
 	ErrRefetchSnapshotChunk = fmt.Errorf("refetch snapshot chunk") // retries after refetching the chunk
-	ErrRetrySnapshot        = fmt.Errorf("retry snapshot")
 	ErrRejectSnapshotChunk  = fmt.Errorf("reject snapshot chunk")
+	// ErrRetrySnapshot        = fmt.Errorf("retry snapshot") // request full retry of snapshot ==> ResponseApplySnapshotChunk_RETRY_SNAPSHOT
 )
 
 func ToABCIOfferSnapshotResponse(err error) abciTypes.ResponseOfferSnapshot_Result {
-
-	if errors.Is(err, nil) {
+	if err == nil {
 		return abciTypes.ResponseOfferSnapshot_ACCEPT
-	} else if errors.Is(err, ErrAbortSnapshot) {
-		return abciTypes.ResponseOfferSnapshot_ABORT
-	} else if errors.Is(err, ErrRejectSnapshot) {
-		return abciTypes.ResponseOfferSnapshot_REJECT
-	} else if errors.Is(err, ErrUnsupportedSnapshotFormat) {
-		return abciTypes.ResponseOfferSnapshot_REJECT_FORMAT
-	} else {
-		return abciTypes.ResponseOfferSnapshot_UNKNOWN
 	}
+
+	if errors.Is(err, ErrAbortSnapshot) {
+		return abciTypes.ResponseOfferSnapshot_ABORT
+	}
+
+	if errors.Is(err, ErrRejectSnapshot) {
+		return abciTypes.ResponseOfferSnapshot_REJECT
+	}
+
+	if errors.Is(err, ErrUnsupportedSnapshotFormat) {
+		return abciTypes.ResponseOfferSnapshot_REJECT_FORMAT
+	}
+
+	return abciTypes.ResponseOfferSnapshot_UNKNOWN
 }
 
 func ToABCIApplySnapshotChunkResponse(err error) abciTypes.ResponseApplySnapshotChunk_Result {
 
-	if errors.Is(err, nil) {
+	if err == nil {
 		return abciTypes.ResponseApplySnapshotChunk_ACCEPT
-	} else if errors.Is(err, ErrAbortSnapshotChunk) {
-		return abciTypes.ResponseApplySnapshotChunk_ABORT
-	} else if errors.Is(err, ErrRetrySnapshotChunk) {
-		return abciTypes.ResponseApplySnapshotChunk_RETRY
-	} else if errors.Is(err, ErrRefetchSnapshotChunk) {
-		return abciTypes.ResponseApplySnapshotChunk_RETRY
-	} else if errors.Is(err, ErrRejectSnapshotChunk) {
-		return abciTypes.ResponseApplySnapshotChunk_REJECT_SNAPSHOT
-	} else {
-		return abciTypes.ResponseApplySnapshotChunk_UNKNOWN
 	}
+
+	if errors.Is(err, ErrAbortSnapshotChunk) {
+		return abciTypes.ResponseApplySnapshotChunk_ABORT
+	}
+
+	if errors.Is(err, ErrRetrySnapshotChunk) {
+		return abciTypes.ResponseApplySnapshotChunk_RETRY
+	}
+
+	if errors.Is(err, ErrRefetchSnapshotChunk) {
+		return abciTypes.ResponseApplySnapshotChunk_RETRY
+	}
+
+	if errors.Is(err, ErrRejectSnapshotChunk) {
+		return abciTypes.ResponseApplySnapshotChunk_REJECT_SNAPSHOT
+	}
+
+	// if errors.Is(err, ErrRetrySnapshot) {
+	// 	return abciTypes.ResponseApplySnapshotChunk_RETRY_SNAPSHOT
+	// }
+
+	// If the error is unrecognized, fall back to rejecting the snapshot.
+	// Returning UNKNOWN is fatal to cometbft.
+	return abciTypes.ResponseApplySnapshotChunk_REJECT_SNAPSHOT
 }
