672722aa0f80660f143dec369148b8b642ee6bf7
Brennan Lamey <brennanlamey@gmail.com>
2024-07-15 11:41:57 -0500

    fix failing tests

--- internal/engine/integration/sql_test.go
+++ internal/engine/integration/sql_test.go
@@ -207,7 +207,7 @@ func Test_SQL(t *testing.T) {
 			name: "inferred type - failure",
 			sql:  "select $id is null",
 			values: map[string]any{
-				"id": "4a67d6ea-7ac8-453c-964e-5a144f9e3004",
+				"$id": "4a67d6ea-7ac8-453c-964e-5a144f9e3004",
 			},
 			err: execution.ErrCannotInferType,
 		},
@@ -215,7 +215,7 @@ func Test_SQL(t *testing.T) {
 			name: "inferred type - success",
 			sql:  "select $id::text is null",
 			values: map[string]any{
-				"id": "4a67d6ea-7ac8-453c-964e-5a144f9e3004",
+				"$id": "4a67d6ea-7ac8-453c-964e-5a144f9e3004",
 			},
 			want: [][]any{{false}},
 		},
