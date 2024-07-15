d2bc4e33d4ce9861ffe3da12c631a601407680d2
Jonathan Chappelow <jon@kwil.com>
2024-07-15 13:16:52 -0500

    version: 0.8.4

--- internal/version/version.go
+++ internal/version/version.go
@@ -15,7 +15,7 @@ import (
 //   - 0.6.0+release
 //   - 0.6.1
 //   - 0.6.2-alpha0+go1.21.nocgo
-const kwilVersion = "0.8.3+release"
+const kwilVersion = "0.8.4+release"
 
 // KwildVersion may be set at compile time by:
 //
