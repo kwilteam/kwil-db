4d569f18acef9d2153303abb04780b3804094d0f
Jonathan Chappelow <jon@kwil.com>
2024-10-03 15:46:53 -0500

    admin: use specified private key with setup init

---
+++ core/utils/files.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"io"
+	"os"
+)
+
+func CopyFile(src, dst string) error {
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	_, err = io.Copy(out, in)
+	return err
+}
