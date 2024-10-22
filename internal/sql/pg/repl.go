e76ad35135dbae134a53f2207eb183caf22a8d19
jchappelow <jon@kwil.com>
2024-10-22 11:40:39 -0500

    pg: recognize streaming prepared tx msg (#1071)

--- internal/sql/pg/repl.go
+++ internal/sql/pg/repl.go
@@ -501,7 +501,7 @@ func decodeWALData(hasher hash.Hash, walData []byte, relations map[uint32]*pglog
 		hasher.Reset()
 		changesetWriter.fail() // discard changeset
 
-	// v2 Stream control messages.  Not expected for kwil
+	// v2 Stream control messages.  Only expected with large transactions.
 	case *pglogrepl.StreamStartMessageV2:
 		*inStream = true
 		logger.Warnf(" [msg] StreamStartMessageV2: xid %d, first segment? %d", logicalMsg.Xid, logicalMsg.FirstSegment)
