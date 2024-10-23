41aa8672ce320573b303384f26d528e16f92f9ed
Jonathan Chappelow <jon@kwil.com>
2024-10-23 10:07:25 -0500

    v2 start

---
+++ v2/node/block.go
@@ -0,0 +1,269 @@
+package node
+
+import (
+	"bytes"
+	"context"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"p2p/node/types"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+)
+
+const (
+	blkReadLimit  = 300_000_000
+	blkGetTimeout = 90 * time.Second
+)
+
+func (n *Node) blkGetStreamHandler(s network.Stream) {
+	defer s.Close()
+
+	req := make([]byte, 128)
+	// io.ReadFull(s, req)
+	nr, err := s.Read(req)
+	if err != nil && err != io.EOF {
+		fmt.Println("bad get blk req", err)
+		return
+	}
+	req, ok := bytes.CutPrefix(req[:nr], []byte(getBlkMsgPrefix))
+	if !ok {
+		fmt.Println("bad get blk request")
+		return
+	}
+	blkid := strings.TrimSpace(string(req))
+	log.Printf("requested blkid: %q", blkid)
+	blkHash, err := types.NewHashFromString(blkid)
+	if err != nil {
+		fmt.Println("invalid block ID", err)
+		return
+	}
+
+	height, rawBlk := n.bki.Get(blkHash)
+	if height == -1 {
+		s.Write(noData) // don't have it
+	} else {
+		binary.Write(s, binary.LittleEndian, height)
+		s.Write(rawBlk)
+	}
+}
+
+func (n *Node) blkAnnStreamHandler(s network.Stream) {
+	defer s.Close()
+
+	s.SetDeadline(time.Now().Add(blkGetTimeout))
+	ctx, cancel := context.WithTimeout(context.Background(), blkGetTimeout)
+	defer cancel()
+
+	req := make([]byte, 256)
+	nr, err := s.Read(req)
+	if err != nil && err != io.EOF {
+		log.Println("bad blk ann req", err)
+		return
+	}
+	req, ok := bytes.CutPrefix(req[:nr], []byte(annBlkMsgPrefix))
+	if !ok {
+		log.Println("bad blk ann request")
+		return
+	}
+	if len(req) <= 64 {
+		log.Println("short blk ann request")
+		return
+	}
+	blkid, after, cut := strings.Cut(string(req), ":")
+	if !cut {
+		log.Println("invalid blk ann request")
+		return
+	}
+	var appHash types.Hash
+	heightStr, appHashStr, cut := strings.Cut(after, ":")
+	if cut {
+		appHash, err = types.NewHashFromString(appHashStr)
+		if err != nil {
+			log.Println("BAD appHash in blk ann request", err)
+			return
+		}
+		log.Println("appHash in blk ann request")
+	}
+
+	blkHash, err := types.NewHashFromString(blkid)
+	if err != nil {
+		log.Printf("invalid block id: %v", err)
+		return
+	}
+	height, err := strconv.ParseInt(heightStr, 10, 64)
+	if err != nil {
+		log.Printf("invalid height in blk ann request: %v", err)
+		return
+	}
+	if height < 0 {
+		log.Printf("invalid height in blk ann request: %d", height)
+		return
+	}
+	log.Printf("blk announcement received: %q / %d", blkid, height)
+
+	// If we are a validator and this is the commit ann for a proposed block
+	// that we already started executing, consensus engine will handle it.
+	if !n.ce.AcceptCommit(height, blkHash, appHash) {
+		return
+	}
+
+	// Possibly ce will handle it regardless.  For now, below is block store
+	// code like a sentry node might do.
+
+	need, done := n.bki.PreFetch(blkHash)
+	defer done()
+	if !need {
+		return // we have or are currently fetching it, do nothing, assuming we have already re-announced
+	}
+
+	log.Printf("retrieving new block: %q", blkid)
+	t0 := time.Now()
+
+	// First try to get from this stream.
+	rawBlk, err := request(s, []byte(getMsg), blkReadLimit)
+	if err != nil {
+		log.Printf("announcer failed to provide %v, trying other peers", blkid)
+		// Since we are aware, ask other peers. we could also put this in a goroutine
+		s.Close() // close the announcers stream first
+		var gotHeight int64
+		gotHeight, rawBlk, err = n.getBlkWithRetry(ctx, blkid, 500*time.Millisecond, 10)
+		if err != nil {
+			log.Printf("unable to retrieve tx %v: %v", blkid, err)
+			return
+		}
+		if gotHeight != height {
+			log.Printf("getblk response had unexpected height: wanted %d, got %d", height, gotHeight)
+			return
+		}
+	}
+
+	log.Printf("obtained content for block %q in %v", blkid, time.Since(t0))
+
+	blk, err := types.DecodeBlock(rawBlk)
+	if err != nil {
+		log.Printf("decodeBlock failed for %v: %v", blkid, err)
+		return
+	}
+	if blk.Header.Height != height {
+		log.Printf("getblk response had unexpected height: wanted %d, got %d", height, blk.Header.Height)
+		return
+	}
+	gotBlkHash := blk.Header.Hash()
+	if gotBlkHash != blkHash {
+		log.Printf("invalid block hash: wanted %v, got %x", blkHash, gotBlkHash)
+		return
+	}
+
+	// re-announce
+
+	go func() {
+		if err := n.ce.CommitBlock(blk, appHash); err != nil {
+			log.Printf("cannot commit announced block: %v", err)
+			return
+		}
+		var appHashStr string
+		if !appHash.IsZero() {
+			appHashStr = appHash.String()
+		}
+		n.announceRawBlk(context.Background(), blkid, height, rawBlk, appHashStr, s.Conn().RemotePeer())
+	}()
+}
+
+func (n *Node) announceBlk(ctx context.Context, blk *types.Block, appHash *types.Hash, from peer.ID) {
+	rawBlk := types.EncodeBlock(blk)
+	var appHashStr string
+	if appHash != nil && !appHash.IsZero() {
+		appHashStr = appHash.String()
+	}
+	n.announceRawBlk(ctx, blk.Hash().String(), blk.Header.Height, rawBlk, appHashStr, from)
+	return
+}
+
+func (n *Node) announceRawBlk(ctx context.Context, blkid string, height int64,
+	rawBlk []byte, appHash string, from peer.ID) {
+	peers := n.peers()
+	if len(peers) == 0 {
+		log.Println("no peers to advertise block to")
+		return
+	}
+
+	for _, peerID := range peers {
+		if peerID == from {
+			continue
+		}
+		log.Printf("advertising block %s (height %d / txs %d) to peer %v", blkid, height, len(rawBlk), peerID)
+		resID := annBlkMsgPrefix + blkid + ":" + strconv.Itoa(int(height))
+		if appHash != "" {
+			resID += ":" + appHash
+		}
+		err := advertiseToPeer(ctx, n.host, peerID, ProtocolIDBlkAnn, contentAnn{resID, []byte(resID), rawBlk})
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+	}
+}
+
+func (n *Node) getBlkWithRetry(ctx context.Context, blkid string, baseDelay time.Duration,
+	maxAttempts int) (int64, []byte, error) {
+	var attempts int
+	for {
+		height, raw, err := n.getBlk(ctx, blkid)
+		if err == nil {
+			return height, raw, nil
+		}
+
+		log.Printf("unable to retrieve block %v (%v), waiting to retry", blkid, err)
+
+		select {
+		case <-ctx.Done():
+		case <-time.After(baseDelay):
+		}
+		baseDelay *= 2
+		attempts++
+		if attempts >= maxAttempts {
+			return 0, nil, ErrBlkNotFound
+		}
+	}
+}
+
+func (n *Node) getBlk(ctx context.Context, blkid string) (int64, []byte, error) {
+	for _, peer := range n.peers() {
+		log.Printf("requesting block %v from %v", blkid, peer)
+		t0 := time.Now()
+		resID := getBlkMsgPrefix + blkid
+		resp, err := requestFrom(ctx, n.host, peer, resID, ProtocolIDBlock, blkReadLimit)
+		if errors.Is(err, ErrNotFound) {
+			log.Printf("block not available on %v", peer)
+			continue
+		}
+		if errors.Is(err, ErrNoResponse) {
+			log.Printf("no response to block request to %v", peer)
+			continue
+		}
+		if err != nil {
+			log.Printf("unexpected error from %v: %v", peer, err)
+			continue
+		}
+
+		if len(resp) < 8 {
+			log.Printf("block response too short")
+			continue
+		}
+
+		log.Printf("obtained content for block %q in %v", blkid, time.Since(t0))
+
+		height := binary.LittleEndian.Uint64(resp[:8])
+		rawBlk := resp[8:]
+
+		return int64(height), rawBlk, nil
+	}
+	return 0, nil, ErrBlkNotFound
+}
---
+++ v2/node/node.go
@@ -0,0 +1,456 @@
+package node
+
+import (
+	"bytes"
+	"context"
+	"crypto/rand"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	mrand2 "math/rand/v2"
+	"path/filepath"
+	"strconv"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"p2p/node/consensus"
+	dummyce "p2p/node/consensus/mock"
+	"p2p/node/mempool"
+	"p2p/node/store"
+	"p2p/node/types"
+
+	"github.com/decred/dcrd/dcrec/secp256k1/v4"
+	"github.com/libp2p/go-libp2p"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/protocol"
+	noise "github.com/libp2p/go-libp2p/p2p/security/noise"
+	"github.com/libp2p/go-libp2p/p2p/transport/tcp"
+	"github.com/multiformats/go-multiaddr"
+	//libp2ptls "github.com/libp2p/go-libp2p/p2p/security/tls"
+)
+
+const (
+	blockTxCount    = 50              // for "mining"
+	dummyTxSize     = 123_000         // for broadcast
+	dummyTxInterval = 1 * time.Second // broadcast freq
+)
+
+type ConsensusEngine interface {
+	Start(context.Context) error
+
+	AcceptProposal(height int64, prevHash types.Hash) bool
+	AcceptCommit(height int64, blkID types.Hash) bool
+	AcceptACK(height int64, blkID types.Hash) bool
+
+	SendConsensusMessage(msg *consensus.ConsensusMessage)
+
+	// Note: Not sure if these are needed here, just for seperate of concerns:
+	// p2p stream handlers role is to downlaod the messages and pass it to the
+	// respective modules to process it and we probably should not be triggering any consensus
+	// affecting methods.
+
+	// ProcessProposal(blk *types.Block, cb func(ack bool, appHash types.Hash) error)
+	// ProcessACK(validatorPK []byte, ack types.AckRes)
+	// CommitBlock(blk *types.Block, appHash types.Hash) error
+}
+
+// parallel CE dev is leading node and p2p dev, so this "Node" currently uses an
+// old interface for a working mock CE. Node code should be updated with final interface
+
+type ConsensusEngineForToyNode interface { // quick and dirty for toy node prototype
+	Start(context.Context) error
+
+	AcceptProposalID(height int64, prevHash types.Hash) bool
+	ProcessProposal(blk *types.Block, cb func(ack bool, appHash *types.Hash) error)
+
+	ProcessACK(validatorPK []byte, ack types.AckRes)
+
+	AcceptCommit(height int64, blkID types.Hash, appHash types.Hash) bool
+	CommitBlock(blk *types.Block, appHash types.Hash) error
+
+	BlockLeaderStream() <-chan *types.QualifiedBlock
+}
+
+type Node struct {
+	bki types.BlockStore
+	mp  types.MemPool
+	ce  ConsensusEngineForToyNode
+
+	pm *peerMan
+	// pf *prefetch
+
+	ackChan chan AckRes // from consensus engine, to gossip to leader
+
+	host   host.Host
+	pex    bool
+	leader atomic.Bool
+	wg     sync.WaitGroup
+	close  func() error
+}
+
+func addClose(close, top func() error) func() error {
+	return func() error { err := top(); return errors.Join(err, close()) }
+}
+
+func NewNode(port uint64, privKey []byte, leader, pex bool) (*Node, error) {
+	close := func() error { return nil }
+
+	host, err := newHost(port, privKey)
+	if err != nil {
+		return nil, err
+	}
+	close = addClose(close, host.Close)
+	// n.host.Network().InterfaceListenAddresses() // expands 0.0.0.0
+
+	mp := mempool.New()
+
+	pm := &peerMan{h: host}
+	if pex {
+		host.SetStreamHandler(ProtocolIDDiscover, pm.discoveryStreamHandler)
+	} else {
+		host.SetStreamHandler(ProtocolIDDiscover, func(s network.Stream) {
+			s.Close()
+		})
+	}
+
+	dir := ".data-" + host.ID().String()
+	dir, _ = filepath.Abs(dir)
+
+	blkStr, err := store.NewBlockStore(dir)
+	if err != nil {
+		return nil, err
+	}
+	close = addClose(close, blkStr.Close)
+
+	ce := dummyce.New(blkStr, mp)
+	ce.BeLeader(leader)
+
+	node := &Node{
+		host:    host,
+		pm:      pm,
+		pex:     pex,
+		mp:      mp,
+		bki:     blkStr,
+		ce:      ce,
+		ackChan: make(chan AckRes, 1),
+		close:   close,
+	}
+
+	node.leader.Store(leader)
+
+	host.SetStreamHandler(ProtocolIDTxAnn, node.txAnnStreamHandler)
+	host.SetStreamHandler(ProtocolIDBlkAnn, node.blkAnnStreamHandler)
+	host.SetStreamHandler(ProtocolIDBlock, node.blkGetStreamHandler)
+	host.SetStreamHandler(ProtocolIDTx, node.txGetStreamHandler)
+
+	host.SetStreamHandler(ProtocolIDBlockPropose, node.blkPropStreamHandler)
+	// host.SetStreamHandler(ProtocolIDACKProposal, node.blkAckStreamHandler)
+
+	return node, nil
+}
+
+func (n *Node) Addr() string {
+	hosts, ports := hostPort(n.host)
+	id := n.host.ID()
+	if len(hosts) == 0 {
+		return ""
+	}
+	return fmt.Sprintf("/ip4/%s/tcp/%d/p2p/%s\n", hosts[0], ports[0], id)
+}
+
+func (n *Node) ID() string {
+	return n.host.ID().String()
+}
+
+// Start begins tx and block gossip, connects to any bootstrap peers, and begins
+// peer discovery.
+func (n *Node) Start(ctx context.Context, peers ...string) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	ps, err := pubsub.NewGossipSub(ctx, n.host)
+	if err != nil {
+		return err
+	}
+	if err := n.startAckGossip(ctx, ps); err != nil { // gossip.go
+		return err
+	}
+
+	// custom stream-based gossip uses txAnnStreamHandler and announceTx.
+	// This dummy method will make create+announce new pretend transactions.
+	// It also periodically rebroadcasts txns.
+	n.startTxAnns(ctx, dummyTxInterval, 30*time.Second, dummyTxSize) // nogossip.go
+
+	// mine is our block anns goroutine, which must be only for leader
+	n.wg.Add(1)
+	go func() {
+		defer n.wg.Done()
+		defer cancel()
+		n.ce.Start(ctx)
+	}()
+
+	// mine is our block anns goroutine, which must be only for leader
+	n.wg.Add(1)
+	go func() {
+		defer n.wg.Done()
+		defer cancel()
+		n.announceBlocks(ctx)
+	}()
+
+	// connect to bootstrap peers, if any
+	for _, peer := range peers {
+		peerInfo, err := connectPeer(ctx, peer, n.host)
+		if err != nil {
+			log.Printf("failed to connect to %v: %v", peer, err)
+			continue
+		}
+		log.Println("connected to ", peerInfo)
+		if err = n.checkPeerProtos(ctx, peerInfo.ID); err != nil {
+			log.Printf("WARNING: peer does not support required protocols %v: %v", peer, err)
+			if err = n.host.Network().ClosePeer(peerInfo.ID); err != nil {
+				log.Printf("failed to disconnect from %v: %v", peer, err)
+			}
+			// n.host.Peerstore().RemovePeer()
+			continue
+		}
+		// n.host.ConnManager().TagPeer(peerID, "validatorish", 1)
+	} // else would use persistent peer store (address book)
+
+	n.wg.Add(1)
+	go func() {
+		defer n.wg.Done()
+		if !n.pex {
+			return
+		}
+		for {
+			// discover for this node
+			peerChan, err := n.pm.FindPeers(ctx, "kwil_namespace")
+			if err != nil {
+				log.Println("FindPeers:", err)
+			} else {
+				go func() {
+					for peer := range peerChan {
+						addPeerToPeerStore(n.host.Peerstore(), peer)
+					}
+				}()
+			}
+
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(20 * time.Second):
+			}
+		}
+	}()
+
+	log.Println("node tx/block gossip started, and peer discovery enabled")
+
+	<-ctx.Done()
+
+	n.wg.Wait()
+
+	return n.close()
+}
+
+func (n *Node) checkPeerProtos(ctx context.Context, peer peer.ID) error {
+	for _, pid := range []protocol.ID{
+		ProtocolIDDiscover,
+		ProtocolIDTx,
+		ProtocolIDTxAnn,
+		ProtocolIDBlock,
+		ProtocolIDBlkAnn,
+		ProtocolIDBlockPropose,
+		// ProtocolIDACKProposal,
+		// pubsub.GossipSubID_v12,
+	} {
+		ok, err := checkProtocolSupport(ctx, n.host, peer, pid)
+		if err != nil {
+			return err
+		}
+		if !ok {
+			return fmt.Errorf("protocol not supported: %v", pid)
+		}
+	}
+	return nil
+}
+
+// announceBlocks announces new committed and proposed blocks when the leader.
+func (n *Node) announceBlocks(ctx context.Context) {
+	blkChan := n.ce.BlockLeaderStream()
+
+	for {
+		var blk *types.QualifiedBlock
+		select {
+		case <-ctx.Done():
+			return
+		case blk = <-blkChan:
+		}
+
+		if blk.Proposed {
+			go n.announceBlkProp(ctx, blk.Block, n.host.ID())
+		} else {
+			go n.announceBlk(ctx, blk.Block, blk.AppHash, n.host.ID())
+		}
+	}
+}
+
+func (n *Node) txGetStreamHandler(s network.Stream) {
+	defer s.Close()
+
+	req := make([]byte, 128)
+	nr, err := s.Read(req)
+	if err != nil && err != io.EOF {
+		fmt.Println("bad get tx req:", err)
+		return
+	}
+	req, ok := bytes.CutPrefix(req[:nr], []byte(getTxMsgPrefix))
+	if !ok {
+		fmt.Println("txGetStreamHandler: bad get tx request", string(req))
+		return
+	}
+	txid := string(req)
+	// log.Printf("requested txid: %q", txid)
+	txHash, err := types.NewHashFromString(txid)
+	if err != nil {
+		fmt.Println("bad txid:", err)
+		return
+	}
+
+	// first check mempool
+	rawTx := n.mp.Get(txHash)
+	if rawTx != nil {
+		s.Write(rawTx)
+		return
+	}
+
+	// this is racy, and should be different in product
+
+	// then confirmed tx index
+	_, rawTx = n.bki.GetTx(txHash)
+	if rawTx == nil {
+		s.Write(noData) // don't have it
+	} else {
+		s.Write(rawTx)
+	}
+
+	// NOTE: response could also include conf/unconf or block height (-1 or N)
+}
+
+type randSrc struct{}
+
+func (randSrc) Uint64() uint64 {
+	var b [8]byte
+	rand.Read(b[:])
+	return binary.LittleEndian.Uint64(b[:])
+}
+
+var rng = mrand2.New(randSrc{})
+
+func (n *Node) peers() []peer.ID {
+	peers := n.host.Network().Peers()
+	rng.Shuffle(len(peers), func(i, j int) {
+		peers[i], peers[j] = peers[j], peers[i]
+	})
+	return peers
+}
+
+func NewKey(r io.Reader) crypto.PrivKey {
+	// priv, _ := secp256k1.GeneratePrivateKeyFromRand(r)
+	// privECDSA := priv.ToECDSA()
+	// privECDSA.Curve = elliptic.P256()
+	// privKey, _, _ := crypto.ECDSAKeyPairFromKey(privECDSA)
+	//privKey, _, err := crypto.GenerateECDSAKeyPair(r)
+	pk, err := secp256k1.GeneratePrivateKeyFromRand(r)
+	if err != nil {
+		panic(err)
+	}
+	privKey := (*crypto.Secp256k1PrivateKey)(pk)
+	// privKey, _, err := crypto.GenerateSecp256k1Key(r)
+	return privKey
+}
+
+func newHost(port uint64, privKey []byte) (host.Host, error) {
+	privKeyP2P, err := crypto.UnmarshalSecp256k1PrivateKey(privKey) // rypto.UnmarshalECDSAPrivateKey(privKey)
+	if err != nil {
+		return nil, err
+	}
+	sourceMultiAddr, _ := multiaddr.NewMultiaddr(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", port))
+
+	// listenAddrs := libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0", "/ip4/0.0.0.0/tcp/0/ws")
+
+	// TODO: use persistent peerstore
+
+	return libp2p.New(
+		libp2p.Transport(tcp.NewTCPTransport),
+		libp2p.Security(noise.ID, noise.New), // modified TLS based on node-ID
+		libp2p.ListenAddrs(sourceMultiAddr),
+		// listenAddrs,
+		libp2p.Identity(privKeyP2P),
+	) // libp2p.RandomIdentity, in-mem peer store, ...
+}
+
+func hostPort(host host.Host) ([]string, []int) {
+	var addrStr []string
+	var ports []int
+	for _, addr := range host.Addrs() { // host.Network().ListenAddresses()
+		ps, _ := addr.ValueForProtocol(multiaddr.P_TCP)
+		port, _ := strconv.Atoi(ps)
+		ports = append(ports, port)
+		as, _ := addr.ValueForProtocol(multiaddr.P_IP4)
+		addrStr = append(addrStr, as)
+
+		log.Println("Listening on", addr)
+	}
+
+	return addrStr, ports
+}
+
+func connectPeer(ctx context.Context, addr string, host host.Host) (*peer.AddrInfo, error) {
+	// Turn the destination into a multiaddr.
+	maddr, err := multiaddr.NewMultiaddr(addr)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	// Extract the peer ID from the multiaddr.
+	info, err := peer.AddrInfoFromP2pAddr(maddr)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	// Add the destination's peer multiaddress in the peerstore.
+	// This will be used during connection and stream creation by libp2p.
+	// host.Peerstore().AddAddrs(info.ID, info.Addrs, peerstore.PermanentAddrTTL)
+
+	return info, host.Connect(ctx, *info)
+}
+
+func checkProtocolSupport(_ context.Context, h host.Host, peerID peer.ID, protoIDs ...protocol.ID) (bool, error) {
+	supported, err := h.Peerstore().SupportsProtocols(peerID, protoIDs...)
+	if err != nil {
+		return false, fmt.Errorf("Failed to check protocols for peer %v: %w", peerID, err)
+	}
+	return len(protoIDs) == len(supported), nil
+
+	// supportedProtos, err := h.Peerstore().GetProtocols(peerID)
+	// if err != nil {
+	// 	return false, err
+	// }
+	// log.Printf("protos supported by %v: %v\n", peerID, supportedProtos)
+
+	// for _, protoID := range protoIDs {
+	// 	if !slices.Contains(supportedProtos, protoID) {
+	// 		return false, nil
+	// 	}
+	// }
+	// return true, nil
+}
