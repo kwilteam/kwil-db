4de7ba671bcdb3dc472bbd50a1084663fd6a3b15
Jonathan Chappelow <jon@kwil.com>
2024-10-31 13:14:18 -0500

    persist addrbook and conn on start

--- v2/node/consensus_test.go
+++ v2/node/consensus_test.go
@@ -2,8 +2,6 @@ package node
 
 import (
 	"bytes"
-	"encoding/binary"
-	"io"
 	"kwil/node/types"
 	"testing"
 	"time"
@@ -58,28 +56,41 @@ func TestBlockProp_MarshalUnmarshal(t *testing.T) {
 				return
 			}
 
-			var newBp blockProp
-			err = newBp.UnmarshalBinary(data)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("UnmarshalBinary() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
+			// Test both MarshalBinary and ReadFrom
+			for _, unmarshalFn := range []func([]byte) (blockProp, error){
+				func(data []byte) (blockProp, error) {
+					var bp blockProp
+					err := bp.UnmarshalBinary(data)
+					return bp, err
+				},
+				func(data []byte) (blockProp, error) {
+					var bp blockProp
+					_, err := bp.ReadFrom(bytes.NewReader(data))
+					return bp, err
+				},
+			} {
+				newBp, err := unmarshalFn(data)
+				if (err != nil) != tt.wantErr {
+					t.Errorf("UnmarshalBinary() error = %v, wantErr %v", err, tt.wantErr)
+					return
+				}
 
-			if !tt.wantErr {
-				if newBp.Height != tt.bp.Height {
-					t.Errorf("Height mismatch: got %v, want %v", newBp.Height, tt.bp.Height)
-				}
-				if newBp.Hash != tt.bp.Hash {
-					t.Errorf("Hash mismatch: got %v, want %v", newBp.Hash, tt.bp.Hash)
-				}
-				if newBp.PrevHash != tt.bp.PrevHash {
-					t.Errorf("PrevHash mismatch: got %v, want %v", newBp.PrevHash, tt.bp.PrevHash)
-				}
-				if newBp.Stamp != tt.bp.Stamp {
-					t.Errorf("Stamp mismatch: got %v, want %v", newBp.Stamp, tt.bp.Stamp)
-				}
-				if !bytes.Equal(newBp.LeaderSig, tt.bp.LeaderSig) {
-					t.Errorf("LeaderSig mismatch: got %v, want %v", newBp.LeaderSig, tt.bp.LeaderSig)
+				if !tt.wantErr {
+					if newBp.Height != tt.bp.Height {
+						t.Errorf("Height mismatch: got %v, want %v", newBp.Height, tt.bp.Height)
+					}
+					if newBp.Hash != tt.bp.Hash {
+						t.Errorf("Hash mismatch: got %v, want %v", newBp.Hash, tt.bp.Hash)
+					}
+					if newBp.PrevHash != tt.bp.PrevHash {
+						t.Errorf("PrevHash mismatch: got %v, want %v", newBp.PrevHash, tt.bp.PrevHash)
+					}
+					if newBp.Stamp != tt.bp.Stamp {
+						t.Errorf("Stamp mismatch: got %v, want %v", newBp.Stamp, tt.bp.Stamp)
+					}
+					if !bytes.Equal(newBp.LeaderSig, tt.bp.LeaderSig) {
+						t.Errorf("LeaderSig mismatch: got %v, want %v", newBp.LeaderSig, tt.bp.LeaderSig)
+					}
 				}
 			}
 		})
@@ -115,39 +126,6 @@ func TestBlockProp_UnmarshalInvalidData(t *testing.T) {
 			err := bp.UnmarshalBinary(tt.data)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("UnmarshalBinary() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func TestBlockProp_UnmarshalFromReader(t *testing.T) {
-	tests := []struct {
-		name    string
-		reader  io.Reader
-		wantErr bool
-	}{
-		{
-			name:    "empty reader",
-			reader:  bytes.NewReader([]byte{}),
-			wantErr: true,
-		},
-		{
-			name: "valid data",
-			reader: bytes.NewReader(func() []byte {
-				buf := make([]byte, 8+2*types.HashLen+8+4)
-				binary.LittleEndian.PutUint64(buf[:8], 100)
-				return buf
-			}()),
-			wantErr: false,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			bp := &blockProp{}
-			err := bp.UnmarshalFromReader(tt.reader)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("UnmarshalFromReader() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
---
+++ v2/node/peers/peers.go
@@ -0,0 +1,386 @@
+package peers
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	"kwil/log"
+	"kwil/node/types"
+
+	"github.com/libp2p/go-libp2p/core/discovery"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/peerstore"
+	"github.com/multiformats/go-multiaddr"
+)
+
+const (
+	maxRetries      = 5
+	baseDelay       = 2 * time.Second
+	disconnectLimit = 7 * 24 * time.Hour // 1 week
+)
+
+type Connector interface {
+	Connect(ctx context.Context, pi peer.AddrInfo) error
+	// ClosePeer(peer.ID) error
+	// Connectedness(peer.ID) network.Connectedness
+	// LocalPeer() peer.ID
+}
+
+// peer/connection manager:
+//	1. manage peerstore (and it's address book)
+//  2. store and load from our address book file
+//	3. provide the Notifee (connect/disconnect hooks)
+//	4. maintain connections (min/max limits)
+
+type RemotePeersFn func(ctx context.Context, peerID peer.ID) ([]peer.AddrInfo, error)
+
+type PeerMan struct {
+	log log.Logger
+	h   host.Host // TODO: remove with just interfaces needed
+	c   Connector
+	ps  peerstore.Peerstore
+
+	requestPeers RemotePeersFn
+
+	pex      bool
+	addrBook string
+
+	done  chan struct{}
+	close func()
+
+	mtx         sync.Mutex
+	retries     map[peer.ID]int       // Track retry attempts for each peer
+	disconnects map[peer.ID]time.Time // Track disconnection timestamps
+}
+
+func NewPeerMan(pex bool, addrBook string, logger log.Logger, h host.Host,
+	requestPeers RemotePeersFn) (*PeerMan, error) {
+	if logger == nil {
+		logger = log.DiscardLogger
+	}
+	done := make(chan struct{})
+	pm := &PeerMan{
+		h:    h, // tmp
+		c:    h,
+		ps:   h.Peerstore(),
+		log:  logger,
+		done: done,
+		close: sync.OnceFunc(func() {
+			close(done)
+		}),
+		pex:          pex,
+		requestPeers: requestPeers,
+		addrBook:     addrBook,
+		retries:      make(map[peer.ID]int),
+		disconnects:  make(map[peer.ID]time.Time),
+	}
+
+	peerInfo, err := loadPeers(pm.addrBook)
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
+		return nil, fmt.Errorf("failed to load address book %s", pm.addrBook)
+	}
+	numPeers := pm.addPeers(peerInfo)
+	logger.Infof("Loaded address book with %d peers", numPeers)
+
+	return pm, nil
+}
+
+var _ discovery.Discoverer = (*PeerMan)(nil) // FindPeers method
+
+func (pm *PeerMan) Start(ctx context.Context) error {
+	var wg sync.WaitGroup
+
+	if pm.pex {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			pm.startPex(ctx)
+		}()
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		pm.removeOldPeers()
+	}()
+
+	<-ctx.Done()
+
+	pm.close()
+
+	wg.Wait()
+
+	return nil
+}
+
+func (pm *PeerMan) startPex(ctx context.Context) {
+	for {
+		// discover for this node
+		peerChan, err := pm.FindPeers(ctx, "kwil_namespace")
+		if err != nil {
+			pm.log.Errorf("FindPeers: %v", err)
+		} else {
+			go func() {
+				var count int
+				for peer := range peerChan {
+					if pm.addPeerAddrs(peer) {
+						// TODO: connection manager, with limits
+						pm.c.Connect(ctx, peer)
+					}
+					count++
+				}
+				if count > 0 {
+					if err := pm.savePeers(); err != nil {
+						pm.log.Warnf("Failed to write address book: %v", err)
+					}
+				}
+			}()
+		}
+
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(20 * time.Second):
+		}
+
+		if err := pm.savePeers(); err != nil {
+			pm.log.Warnf("Failed to write address book: %v", err)
+		}
+	}
+}
+
+func (pm *PeerMan) FindPeers(ctx context.Context, ns string, opts ...discovery.Option) (<-chan peer.AddrInfo, error) {
+	peerChan := make(chan peer.AddrInfo)
+
+	peers := pm.h.Network().Peers()
+	if len(peers) == 0 {
+		close(peerChan)
+		pm.log.Warn("no existing peers for peer discovery")
+		return peerChan, nil
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(peers))
+	for _, peerID := range peers {
+		go func() {
+			defer wg.Done()
+			ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+			defer cancel()
+			peers, err := pm.requestPeers(ctx, peerID)
+			if err != nil {
+				fmt.Printf("Failed to get peers from %v: %v", peerID, err)
+				return
+			}
+
+			for _, p := range peers {
+				peerChan <- p
+			}
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(peerChan)
+	}()
+
+	return peerChan, nil
+}
+
+func (pm *PeerMan) KnownPeers() []types.PeerInfo {
+	var peers []types.PeerInfo
+	for _, peerID := range pm.h.Network().Peers() { // connected peers only
+		addrs := pm.ps.Addrs(peerID)
+
+		supportedProtos, err := pm.ps.GetProtocols(peerID)
+		if err != nil {
+			fmt.Printf("GetProtocols for %v: %v\n", peerID, err)
+			continue
+		}
+
+		peers = append(peers, types.PeerInfo{
+			AddrInfo: types.AddrInfo{
+				ID:    peerID,
+				Addrs: addrs,
+			},
+			Protos: supportedProtos,
+		})
+
+	}
+	return peers
+}
+
+func (pm *PeerMan) printKnownPeers() {
+	peers := pm.KnownPeers()
+	for _, p := range peers {
+		pm.log.Info("Known peer", "id", p.ID.String())
+	}
+}
+
+func (pm *PeerMan) savePeers() error {
+	peerList := pm.KnownPeers()
+	if err := persistPeers(peerList, pm.addrBook); err != nil {
+		return err
+	}
+	return nil
+}
+
+// persistPeers saves known peers to a JSON file
+func persistPeers(peers []types.PeerInfo, filePath string) error {
+	// Marshal peerList to JSON
+	data, err := json.MarshalIndent(peers, "", "  ")
+	if err != nil {
+		return fmt.Errorf("marshaling peers to JSON: %v", err)
+	}
+
+	// Write to file
+	err = os.WriteFile(filePath, data, 0644)
+	if err != nil {
+		return fmt.Errorf("writing peers to file: %w", err)
+	}
+	return nil
+}
+
+func loadPeers(filePath string) ([]types.PeerInfo, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read peerstore file: %w", err)
+	}
+
+	var peerList []types.PeerInfo
+	if err := json.Unmarshal(data, &peerList); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal peerstore data: %w", err)
+	}
+	return peerList, nil
+}
+
+func (pm *PeerMan) addPeers(peerList []types.PeerInfo) int {
+	var count int
+	for _, pInfo := range peerList {
+		// for _, addr := range pInfo.Addrs {
+		// 	ps.AddAddr(pInfo.ID, addr, peerstore.PermanentAddrTTL)
+		// }
+		addrs := pm.ps.Addrs(pInfo.ID)
+		for _, addr := range pInfo.Addrs {
+			if !multiaddr.Contains(addrs, addr) {
+				pm.ps.AddAddr(pInfo.ID, addr, time.Hour)
+				pm.log.Infof("Added new peer address to store: %v @ %v", pInfo.ID, addr)
+				count++
+			}
+			// TODO: we need a connect hook to change to forever on connect
+		}
+		//addPeerAddrs(ps, peer.AddrInfo(pInfo.AddrInfo))
+		for _, proto := range pInfo.Protos {
+			if err := pm.ps.AddProtocols(pInfo.ID, proto); err != nil {
+				pm.log.Warnf("Error adding protocol %s for peer %s: %v", proto, pInfo.ID, err)
+			}
+		}
+	}
+
+	return count
+}
+
+// addPeerAddrs adds a discovered peer to the local peer store.
+func (pm *PeerMan) addPeerAddrs(p peer.AddrInfo) (added bool) {
+	pm.addPeers([]types.PeerInfo{
+		{
+			AddrInfo: types.AddrInfo(p),
+			// No known protocols, yet
+		},
+	})
+	return added
+}
+
+// Connected is triggered when a peer connects
+func (pm *PeerMan) Connected(net network.Network, conn network.Conn) {
+	peerID := conn.RemotePeer()
+	pm.log.Infof("Connected to peer %s", peerID)
+
+	// Reset retries and disconnect timestamp on successful connection
+	pm.mtx.Lock()
+	defer pm.mtx.Unlock()
+	pm.retries[peerID] = 0
+	delete(pm.disconnects, peerID)
+}
+
+// Disconnected is triggered when a peer disconnects
+func (pm *PeerMan) Disconnected(net network.Network, conn network.Conn) {
+	peerID := conn.RemotePeer()
+	pm.log.Infof("Disconnected from peer %v", peerID)
+	// Store disconnection timestamp
+	pm.mtx.Lock()
+	defer pm.mtx.Unlock()
+	pm.disconnects[peerID] = time.Now()
+	go pm.reconnectWithRetry(peerID)
+}
+
+func (pm *PeerMan) Listen(network.Network, multiaddr.Multiaddr)      {}
+func (pm *PeerMan) ListenClose(network.Network, multiaddr.Multiaddr) {}
+
+// Reconnect logic with retry
+
+// Reconnect logic with exponential backoff and capped retries
+func (pm *PeerMan) reconnectWithRetry(peerID peer.ID) {
+	for attempt := 1; attempt <= maxRetries; attempt++ {
+		addrInfo := peer.AddrInfo{
+			ID:    peerID,
+			Addrs: pm.ps.Addrs(peerID),
+		}
+
+		delay := baseDelay * (1 << (attempt - 1))
+		if delay > 1*time.Minute {
+			delay = 1 * time.Minute // Cap delay at 1 minute
+		}
+
+		pm.log.Infof("Attempting reconnection to peer %s (attempt %d/%d)", peerID, attempt, maxRetries)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		if err := pm.c.Connect(ctx, addrInfo); err != nil {
+			cancel()
+			pm.log.Infof("Failed to reconnect to peer %s (trying again in %v): %v", peerID, delay, err)
+		} else {
+			cancel()
+			pm.log.Infof("Successfully reconnected to peer %s", peerID)
+			return
+		}
+
+		select {
+		case <-pm.done:
+			return
+		case <-time.After(delay):
+		}
+	}
+	pm.log.Infof("Exceeded max retries for peer %s. Giving up.", peerID)
+}
+
+// Periodically remove peers disconnected for over a week
+func (pm *PeerMan) removeOldPeers() {
+	ticker := time.NewTicker(10 * time.Minute)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-pm.done:
+			return
+		case <-ticker.C:
+		}
+
+		now := time.Now()
+		func() {
+			pm.mtx.Lock()
+			defer pm.mtx.Unlock()
+			for peerID, disconnectTime := range pm.disconnects {
+				if now.Sub(disconnectTime) > disconnectLimit {
+					pm.ps.RemovePeer(peerID)
+					delete(pm.disconnects, peerID) // Remove from tracking map
+					pm.log.Infof("Removed peer %s last connected %v ago", peerID, time.Since(disconnectTime))
+				}
+			}
+		}()
+	}
+}
