927f9097eca6e380f747c650d7d934202217e973
Jon Chappelow <jon@kwil.com>
2025-03-13 10:58:15 -0500

    tx send queue per peer

--- node/nogossip.go
+++ node/nogossip.go
@@ -3,8 +3,8 @@ package node
 import (
 	"context"
 	"errors"
-	"fmt"
 	"io"
+	"sync"
 	"time"
 
 	ktypes "github.com/kwilteam/kwil-db/core/types"
@@ -84,14 +84,15 @@ func (n *Node) txAnnStreamHandler(s network.Stream) {
 	}
 
 	// re-announce
-	n.queueTxn(txHash, rawTx, s.Conn().RemotePeer())
+	n.queueTxn(ctx, txHash, rawTx, s.Conn().RemotePeer())
 }
 
-func (n *Node) queueTxn(txID types.Hash, rawTx []byte, from peer.ID) {
+func (n *Node) queueTxn(ctx context.Context, txID types.Hash, rawTx []byte, from peer.ID) {
 	tx := orderedTxn{txID: txID, rawtx: rawTx, from: from}
 
 	select {
 	case n.txQueue <- tx:
+	case <-ctx.Done():
 	default:
 		n.log.Warnf("tx queue full, dropping tx %v", txID)
 	}
@@ -108,45 +109,151 @@ func (n *Node) announceRawTx(ctx context.Context, txHash types.Hash, rawTx []byt
 		if peerID == from {
 			continue
 		}
-		// n.log.Infof("advertising tx %v (len %d) to peer %v", txid, len(rawTx), peerID)
-		err := n.advertiseTxToPeer(ctx, peerID, txHash, rawTx)
-		if err != nil {
-			n.log.Warn("failed to advertise tx to peer", "peer", peerID, "error", err)
-			continue
-		}
+		n.log.Infof("advertising tx %v (len %d) to peer %v", txHash, len(rawTx), peerID)
+		n.advertiseTxToPeer(ctx, peerID, txHash, rawTx)
 	}
 }
 
-func (n *Node) announceTx(_ context.Context, _ *ktypes.Transaction, txID types.Hash, from peer.ID) {
+func (n *Node) announceTx(ctx context.Context, txID types.Hash, from peer.ID) {
 	// Storing nil for the raw transaction as it is already in the mempool and can be retrieved
 	// when dequeued. This helps in reducing the memory footprint of the queue.
-	n.queueTxn(txID, nil, from)
+	n.queueTxn(ctx, txID, nil, from)
+}
+
+type txSendQueue struct {
+	mtx    sync.Mutex
+	queues map[peer.ID]*signallingQueue
+	cp     *sync.Pool
+}
+
+func (n *Node) peerQueue(peerID peer.ID) (<-chan struct{}, func(), bool) {
+	n.sendQueue.mtx.Lock()
+	defer n.sendQueue.mtx.Unlock()
+	if n.sendQueue.cp == nil {
+		n.sendQueue.cp = &sync.Pool{
+			New: func() any {
+				return make(chan struct{}, 1)
+			},
+		}
+	}
+	q, ok := n.sendQueue.queues[peerID]
+	if !ok {
+		q = newSignallingQueue(nil) // n.sendQueue.cp
+		if n.sendQueue.queues == nil {
+			n.sendQueue.queues = make(map[peer.ID]*signallingQueue)
+		}
+		n.sendQueue.queues[peerID] = q
+	}
+	return q.tryEnqueue()
+}
+
+const txQueueCap = 4000
+
+type signallingQueue struct {
+	mtx    sync.Mutex
+	active bool
+	q      []chan struct{}
+	// cursor int // TODO: circular buffer indexing of q
+	// cap    int // commented to use global txQueueCap for now
+
+	// a pool of channels to avoid allocations, may be shared across many queues
+	cp *sync.Pool // must be *buffered* (1) chan struct{}
+}
+
+func newSignallingQueue(cp *sync.Pool) *signallingQueue {
+	return &signallingQueue{
+		cp: cp,
+	}
+}
+
+func (sg *signallingQueue) tryEnqueue() (<-chan struct{}, func(), bool) {
+	sg.mtx.Lock()
+	defer sg.mtx.Unlock()
+
+	if sg.active && len(sg.q) >= txQueueCap {
+		return nil, nil, false // full, can't get in queue
+	}
+
+	if sg.cp == nil { // internal (unshared) pool, not yet initialized
+		sg.cp = &sync.Pool{
+			New: func() any {
+				return make(chan struct{}, 1)
+			},
+		}
+	}
+
+	c := sg.cp.Get().(chan struct{})
+
+	if sg.active {
+		sg.q = append(sg.q, c)
+	} else {
+		sg.active = true
+		c <- struct{}{}
+	}
+
+	return c, func() {
+		// trigger the next and return this channel to the pool
+		sg.mtx.Lock()
+		defer sg.mtx.Unlock()
+		sg.next()
+		sg.cp.Put(c)
+	}, true
+}
+
+func (sg *signallingQueue) next() {
+	if len(sg.q) == 0 { // nothing waiting, go back to sleep
+		sg.active = false
+		return
+	}
+	// signal to the next
+	c := sg.q[0]
+	sg.q = sg.q[1:]
+	c <- struct{}{}
+	sg.active = true // should be true already, but just in case
 }
 
 // advertiseTxToPeer sends a lightweight advertisement to a connected peer.
 // The stream remains open in case the peer wants to request the content right.
-func (n *Node) advertiseTxToPeer(ctx context.Context, peerID peer.ID, txHash types.Hash, rawTx []byte) error {
-	s, err := n.host.NewStream(ctx, peerID, ProtocolIDTxAnn)
-	if err != nil {
-		return fmt.Errorf("failed to open stream to peer: %w", peers.CompressDialError(err))
+func (n *Node) advertiseTxToPeer(ctx context.Context, peerID peer.ID, txHash types.Hash, rawTx []byte) {
+	start, done, ok := n.peerQueue(peerID) // queueing is synchronous; p2p stream is async but sequential
+	if !ok {
+		n.log.Warnf("peer queue full, dropping tx advertisement for %s", peers.PeerIDStringer(peerID))
+		return
 	}
 
-	roundTripDeadline := time.Now().Add(txAnnTimeout)
-	s.SetWriteDeadline(roundTripDeadline)
+	// Probably a better approach would be to only start the goroutines when
+	// ready. This would require a master goroutine somewhere to start the
+	// goroutines using the stored peer ID and tx info. It's easier for now to
+	// capture the info in a closure here.
 
-	// Send a lightweight advertisement with the object ID
-	_, err = newTxHashAnn(txHash).WriteTo(s)
-	if err != nil {
-		return fmt.Errorf("txann failed to peer %s: %w", peerID, err)
-	}
-
-	mets.Advertised(ctx, string(ProtocolIDTxAnn))
-
-	// n.log.Infof("advertised tx content %s to peer %s", txid, peerID)
-
-	// Keep the stream open for potential content requests
 	go func() {
+		select {
+		case <-start:
+			defer done()
+		case <-ctx.Done():
+			return
+		}
+
+		s, err := n.host.NewStream(ctx, peerID, ProtocolIDTxAnn)
+		if err != nil {
+			n.log.Warnf("failed to open stream to peer: %w", peers.CompressDialError(err))
+			return
+		}
 		defer s.Close()
+
+		roundTripDeadline := time.Now().Add(txAnnTimeout)
+		s.SetWriteDeadline(roundTripDeadline)
+
+		// Send a lightweight advertisement with the object ID
+		_, err = newTxHashAnn(txHash).WriteTo(s)
+		if err != nil {
+			n.log.Warnf("txann failed to peer %s: %w", peerID, err)
+			return
+		}
+
+		mets.Advertised(ctx, string(ProtocolIDTxAnn))
+
+		// n.log.Infof("advertised tx content %s to peer %s", txid, peerID)
 
 		s.SetReadDeadline(time.Now().Add(txAnnRespTimeout))
 
@@ -166,13 +273,13 @@ func (n *Node) advertiseTxToPeer(ctx context.Context, peerID peer.ID, txHash typ
 			return
 		}
 
+		// we could queue at this level too
+
 		s.SetWriteDeadline(time.Now().Add(txGetTimeout))
 		s.Write(rawTx)
 
 		mets.AdvertiseServed(ctx, string(ProtocolIDTxAnn), int64(len(rawTx)))
 	}()
-
-	return nil
 }
 
 // startTxAnns handles periodic reannouncement. It can also be modified to
@@ -204,7 +311,7 @@ func (n *Node) startTxAnns(ctx context.Context, reannouncePeriod time.Duration) 
 				var numSent, bytesSent int64
 				for _, tx := range txns {
 					rawTx := tx.Bytes()
-					n.queueTxn(tx.Hash(), rawTx, n.host.ID()) // response handling is async
+					n.queueTxn(ctx, tx.Hash(), rawTx, n.host.ID()) // response handling is async
 					if ctx.Err() != nil {
 						n.log.Warn("interrupting long re-broadcast")
 						break
